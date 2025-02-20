799bee5865b0b50a644535bd9dcacaf667d4b4b7
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-02-20 15:25:17 -0500

    feat: Add Metric component

---
+++ src/components/Metric.tsx
@@ -0,0 +1,22 @@
+export interface MetricProps {
+    /** The figure to display, eg. `#10` or `3500+` */
+    figure: string;
+    /** The caption under the figure, eg. `Ranking` or `EECS students` */
+    caption?: string | undefined;
+    /** Whether to add a coloured border around the component */
+    border?: boolean | undefined;
+    /** Additional classes added to the metric container */
+    className?: string | undefined;
+}
+
+const Metric = ({ figure, caption, border, className }: MetricProps) => {
+    const baseStyle = 'py-8 px-12 text-center w-min h-min';
+    return (
+        <div className={`${baseStyle} ${className || ''} ${border && 'outline-gradient'}`}>
+            <span className="font-heading text-3xl">{figure}</span>
+            {caption && <p className="font-sans whitespace-nowrap text-lg text-gray-500">{caption}</p>}
+        </div>
+    )
+}
+
+export default Metric;
