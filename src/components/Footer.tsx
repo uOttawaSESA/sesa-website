bff12d5abd772542282de76b87b7feb6bf4b8157
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-02-20 11:48:02 -0500

    feat: Remake footer for new design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,109 +1,80 @@
-import Image from "next/image";
 import Button from "./Button";
 
 export default function Footer() {
     return (
-        <footer className="mx-8 xl:mx-32 2xl:mx-64 my-8 bg-gray">
-            <div className="flex flex-col lg:flex-row justify-between [backdrop-filter:blur(100px)]">
-                <div className="flex items-center justify-center gap-4">
-                    <Image
-                        className="my-0"
-                        width="50"
-                        height="50"
-                        src="sesa-logo.svg"
-                        alt="SESA Logo"
-                    />
-                    <Image
-                        className="my-0"
-                        width="109"
-                        height="25"
-                        src="logo-text.svg"
-                        alt="SESA Logo Text"
-                    />
+        <footer className="bg-gray mx-8 my-8 flex flex-col gap-4 xl:mx-32 2xl:mx-64">
+            <div className="grid grid-cols-1 gap-4 text-center sm:grid-cols-2 lg:grid-cols-4">
+                <div className="flex flex-col gap-4">
+                    <h3 className="color-gradient font-mono">Contact</h3>
+                    <p className="font-mono text-gray-400">
+                        800 King Edward Ave,
+                        <br />
+                        Ottawa, ON, K1N 1A2,
+                        <br />
+                        STE 0109
+                        <br />
+                        uottawa.sesa@gmail.com
+                    </p>
                 </div>
-                <div className="flex flex-col md:flex-row md:gap-12 justify-center items-center">
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Team
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Events
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Resources
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Contact
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Sponsor
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Privacy &amp; Terms
-                    </a>
+                <div className="flex flex-col gap-4">
+                    <h3 className="color-gradient font-mono">Legal</h3>
+                    <ul className="flex flex-col gap-4">
+                        <li>
+                            <a className="font-mono text-xl uppercase" href="#">
+                                Privacy Policy
+                            </a>
+                        </li>
+                        <li>
+                            <a className="font-mono text-xl uppercase" href="#">
+                                Terms of Service
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-            </div>
-            <div className="flex items-center justify-between">
-                <p>
-                    &copy; 2014&ndash;{new Date().getFullYear()} uOttawa Software Engineering
-                    Student Association (SESA)
-                </p>
-                <div className="flex gap-4">
-                    <Button href="#" variant="outline" className="!p-3">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </Button>
-                    <Button href="#" variant="outline" className="!p-3">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </Button>
-                    <Button href="#" variant="outline" className="!p-3">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </Button>
-                    <Button href="#" variant="outline" className="!p-3">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </Button>
-                    <Button href="#" variant="outline" className="!p-3">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </Button>
-                    <Button href="#" variant="outline" className="!p-3">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </Button>
+                <div className="flex flex-col gap-4">
+                    <h3 className="color-gradient font-mono">Navigation</h3>
+                    <ul className="flex flex-col gap-4">
+                        <li>
+                            <a className="font-mono text-xl uppercase" href="#">
+                                Team
+                            </a>
+                        </li>
+                        <li>
+                            <a className="font-mono text-xl uppercase" href="#">
+                                Events
+                            </a>
+                        </li>
+                        <li>
+                            <a className="font-mono text-xl uppercase" href="#">
+                                Resources
+                            </a>
+                        </li>
+                        <li>
+                            <a className="font-mono text-xl uppercase" href="#">
+                                Contact
+                            </a>
+                        </li>
+                        <li>
+                            <a className="font-mono text-xl uppercase" href="#">
+                                Sponsor
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <h3 className="color-gradient font-mono">Socials</h3>
+                    <div className="flex justify-center gap-2">
+                        <Button className="!p-3">I</Button>
+                        <Button className="!p-3">D</Button>
+                        <Button className="!p-3">L</Button>
+                        <Button className="!p-3">Y</Button>
+                    </div>
                 </div>
             </div>
+            <p>
+                &copy; 2014&ndash;{new Date().getFullYear()} uOttawa Software Engineering Student
+                Association (SESA)
+            </p>
         </footer>
     );
 }
