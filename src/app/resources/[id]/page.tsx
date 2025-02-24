c7f90b84e5d4916a7e89a078fa781c21755bef78
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-02-23 22:06:07 -0500

    feat: Add other resources to bottom of page
    There are still some styling issues, but this mostly works. Scrolling is
    a bit broken if a user presses the button again before the first scroll
    finishes, so that might be something to fix. I'd like to keep smooth
    scrolling, though.

--- src/app/resources/[id]/page.tsx
+++ src/app/resources/[id]/page.tsx
@@ -2,8 +2,12 @@ import { notFound } from "next/navigation";
 import DOMPurify from "dompurify";
 import { JSDOM } from "jsdom";
 import { marked } from "marked";
-import { use, useMemo } from "react";
+import { use, useMemo, useRef } from "react";
 import Button from "@/components/Button";
+import IconButton from "@/components/IconButton";
+import Image from "next/image";
+
+import OtherResources from "./OtherResources";
 
 export interface Resource {
     /** Tile of the resource. */
@@ -71,47 +75,54 @@ export default function ResourcePage({ params }: { params: Promise<{ id: string 
     const resourceDate = new Date(resource.date);
 
     return (
-        <div className="relative my-16 flex justify-center">
-            <Button
-                variant="outline"
-                className="absolute left-72 top-0 font-heading uppercase"
-                href="/pages/ResourcesPage"
-            >
-                Go Back
-            </Button>
-            <div className="flex min-w-[65ch] flex-col gap-32">
-                {/* Resource */}
-                <article>
-                    {/* Tags */}
-                    <div className="mb-2 flex gap-4 font-mono uppercase">
-                        {resource.tags.map((tag, i) => (
-                            <span key={i} className="fill-gradient p-2">
-                                {tag}
-                            </span>
-                        ))}
-                    </div>
-                    {/* Title, author, and date */}
-                    <h1 className="font-heading text-4xl uppercase">{resource.title}</h1>
-                    {resource.author && (
-                        <p className="color-gradient font-mono text-lg">{resource.author}</p>
-                    )}
-                    <time className="text-md font-mono" dateTime={resourceDate.toISOString()}>
-                        {resourceDate.toLocaleDateString()}
-                    </time>
-                    {/* Markdown-parsed body */}
-                    <p className="markdown" dangerouslySetInnerHTML={{ __html: resourceBody }} />
-                </article>
-                {/* Rating */}
-                <div className="outline-gradient flex min-w-[65ch] justify-between p-5">
-                    <p className="font-heading uppercase">Is this resource helpful?</p>
-                    <div className="flex gap-1 font-mono">
-                        <a href="#">(thumbs up)</a>
-                        <span>25</span>
-                        <a href="#">(thumbs down)</a>
-                        <span>0</span>
+        <>
+            <div className="relative my-16 flex justify-center">
+                <Button
+                    variant="outline"
+                    className="absolute left-72 top-0 font-heading uppercase"
+                    href="/pages/ResourcesPage"
+                >
+                    Go Back
+                </Button>
+                <div className="flex min-w-[65ch] flex-col gap-32">
+                    {/* Resource */}
+                    <article>
+                        {/* Tags */}
+                        <div className="mb-2 flex gap-4 font-mono uppercase">
+                            {resource.tags.map((tag, i) => (
+                                <span key={i} className="fill-gradient p-2">
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+                        {/* Title, author, and date */}
+                        <h1 className="font-heading text-4xl uppercase">{resource.title}</h1>
+                        {resource.author && (
+                            <p className="color-gradient font-mono text-lg">{resource.author}</p>
+                        )}
+                        <time className="text-md font-mono" dateTime={resourceDate.toISOString()}>
+                            {resourceDate.toLocaleDateString()}
+                        </time>
+                        {/* Markdown-parsed body */}
+                        <p
+                            className="markdown"
+                            dangerouslySetInnerHTML={{ __html: resourceBody }}
+                        />
+                    </article>
+                    {/* Rating */}
+                    <div className="outline-gradient flex min-w-[65ch] justify-between p-5">
+                        <p className="font-heading uppercase">Is this resource helpful?</p>
+                        <div className="flex gap-1 font-mono">
+                            <a href="#">(thumbs up)</a>
+                            <span>25</span>
+                            <a href="#">(thumbs down)</a>
+                            <span>0</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {/* Other Resources */}
+            <OtherResources />
+        </>
     );
 }
