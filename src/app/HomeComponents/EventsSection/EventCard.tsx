eabfdcf46c4a46130760022ee6f383086ad4de64
tazim04 <35643104+tazim04@users.noreply.github.com>
2025-03-01 13:51:33 -0500

    events section complete on home page, added a new color-gradient-clickable class in global.css with a hover effect

--- src/app/HomeComponents/EventsSection/EventCard.tsx
+++ src/app/HomeComponents/EventsSection/EventCard.tsx
@@ -1,31 +1,37 @@
 import Image from "next/image";
 import { format } from "date-fns";
+import { Event } from "@/app/pages/EventsPage/utils/types";
+import { useRef, useState, useEffect } from "react";
 
-interface EventCardProps {
-    title: string;
-    date: Date;
-    startTime: Date;
-    endTime: Date;
-    location: string;
-    description: string;
-    imgPath: string;
-    details: string;
-}
-
-const EventCard: React.FC<EventCardProps> = ({
+const EventCard: React.FC<Event> = ({
     title,
     description,
     date,
     startTime,
     endTime,
     location,
-    imgPath,
-    details,
+    image,
+    instagramLink,
 }) => {
     const formattedDate = format(date, "MMM dd, yyyy"); // e.g., "Mar 25, 2024"
     const day = format(date, "dd"); // e.g., "25"
     const month = format(date, "MMM"); // e.g., "Mar"
     const timeRange = `${format(startTime, "ha")} – ${format(endTime, "ha")}`; // "6PM – 8PM"
+
+    // Track title height to dynamically switch line-clamp of the description
+    const titleRef = useRef<HTMLHeadingElement>(null);
+    const [titleHeight, setTitleHeight] = useState(0);
+
+    // Effect to measure the title height
+    useEffect(() => {
+        if (titleRef.current) {
+            setTitleHeight(titleRef.current.clientHeight);
+            console.log("Title Height:", titleRef.current.clientHeight);
+        }
+    }, [title]);
+
+    // Determine line-clamp for description based on title height
+    const descriptionLineClamp = titleHeight >= 56 ? "line-clamp-4" : "line-clamp-6"; // Adjust the height threshold as needed
 
     return (
         <div className="relative h-[667px] w-[361px] max-w-sm overflow-hidden border-[1.5px] border-solid border-blueviolet-700 bg-gray-200 opacity-90">
@@ -33,7 +39,7 @@ const EventCard: React.FC<EventCardProps> = ({
             <a href="#">
                 <Image
                     className="border-b-[1.5px] border-solid border-blueviolet-700 object-cover"
-                    src={imgPath}
+                    src={image}
                     alt={title}
                     width={361}
                     height={361}
@@ -62,15 +68,25 @@ const EventCard: React.FC<EventCardProps> = ({
                 </div>
 
                 {/* Title */}
-                <a href="#">
-                    <h5 className="mb-2 text-lg uppercase tracking-tight">{title}</h5>
-                </a>
+                <h5 className="mb-2 text-lg uppercase tracking-tight" ref={titleRef}>
+                    {title}
+                </h5>
 
                 {/* Description */}
-                <p className="mb-3 font-sans text-base text-gray-400">{description}</p>
+                <div className="h-28">
+                    <p
+                        className={`mb-3 ${descriptionLineClamp} overflow-hidden text-ellipsis font-sans text-base text-gray-400`}
+                    >
+                        {description}
+                    </p>
+                </div>
 
                 <div className="absolute bottom-5 right-5">
-                    <a href={details} className="color-gradient text-lg text-transparent">
+                    <a
+                        href={instagramLink}
+                        target="_blank"
+                        className="color-gradient-clickable text-lg text-transparent"
+                    >
                         Details
                     </a>
                 </div>
--- src/app/HomeComponents/EventsSection/EventsCarousel.tsx
+++ src/app/HomeComponents/EventsSection/EventsCarousel.tsx
@@ -2,67 +2,9 @@
 
 import EventCard from "./EventCard";
 import { useEffect, useRef } from "react";
+import { events } from "@/app/pages/EventsPage/utils/eventData";
 
 const CARD_WIDTH = 350;
-
-const events = [
-    {
-        title: "Microsoft Crafting Custom Chatbots",
-        date: new Date("2024-03-25"),
-        startTime: new Date("2024-03-25T18:00:00"),
-        endTime: new Date("2024-03-25T20:00:00"),
-        location: "STM 117",
-        description:
-            "Calling all Tech Enthusiasts! SESA is hosting an engaging AI and LLM workshop with uOttawa’s Microsoft Student Ambassador...",
-        imgPath: "/imgs/Events/chatbots.png",
-        details: "#",
-    },
-    {
-        title: "SESA Social",
-        date: new Date("2024-11-11"),
-        startTime: new Date("2024-11-11T16:00:00"),
-        endTime: new Date("2024-11-11T20:00:00"),
-        location: "Room 210",
-        description:
-            "WE’RE BACK! Unlock the fun at our SESA Social Event on March 11 🎉 Swing by STM117 from 6-8 PM for an evening of games, laughter, and new connections!",
-        imgPath: "/imgs/Events/social.png",
-        details: "#",
-    },
-    {
-        title: "SESA Social 2",
-        date: new Date("2024-11-02"),
-        startTime: new Date("2024-11-02T19:00:00"),
-        endTime: new Date("2024-11-02T21:00:00"),
-        location: "Auditorium",
-        description:
-            "YOU'RE INVITED! Come to SESA's Social for an evening of fun with video games, board games, free food, and more 🥳 REGISTER NOW with the link in our bio!",
-        imgPath: "/imgs/Events/social2.png",
-        details: "#",
-    },
-    {
-        title: "POWER UP YOUR NETWORK WITH CIENA!",
-        date: new Date("2024-11-02"),
-        startTime: new Date("2024-11-02T19:00:00"),
-        endTime: new Date("2024-11-02T21:00:00"),
-        location: "STM 117",
-        description: `SOLD OUT —
-                    🌐 Join us for an unforgettable networking experience! 🤝 Ciena...
-                    2nd Annual Networking Event is`,
-        imgPath: "/imgs/Events/ciena.png",
-        details: "#",
-    },
-    {
-        title: "INTRODUCTION TO UNITY",
-        date: new Date("2024-11-02"),
-        startTime: new Date("2024-11-02T19:00:00"),
-        endTime: new Date("2024-11-02T21:00:00"),
-        location: "STM 117",
-        description:
-            "Calling all aspiring game developers! Get ready to level up your game development skills at the #GotGames introduction to Unity workshop! Join @hackthehill and @uogamedev on January 25th from 7pm to 9pm in STM117 and on Twitch.",
-        imgPath: "/imgs/Events/hackthehill.png",
-        details: "#",
-    },
-];
 
 const EventsCarousel = ({
     setScrollFunction,
@@ -87,7 +29,7 @@ const EventsCarousel = ({
     };
 
     return (
-        <div className="mt-14 flex flex-col overflow-hidden">
+        <div className="mt-12 flex flex-col overflow-hidden">
             {/* Scrollable Event Cards */}
             <div className="relative w-full">
                 <div
