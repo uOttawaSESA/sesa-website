69744dee05287597c25334947456fbe7aac3d146
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-03-05 14:59:10 -0500

    feat: Prepare "Resources" page for i18n

--- src/app/[locale]/ResourcesPage/components/FooterSection.tsx
+++ src/app/[locale]/ResourcesPage/components/FooterSection.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import Button from "@/components/Button";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 const FooterSection: React.FC = () => {
+    const t = useTranslations("resources");
+
     return (
         <>
             {/* CTA Section */}
             <div className="mt-16 flex items-center justify-center gap-6">
                 <div className="flex flex-col items-center text-center">
                     <h2 className="text-l mb-8 font-heading uppercase">
-                        Interested in Contributing or Requesting Resources?
+                        {t("contributing_heading")}
                     </h2>
                 </div>
                 <div className="flex -translate-y-4 transform items-center justify-center">
                     <Button href="#" className="font-heading text-xl uppercase">
-                        Join Our Discord
+                        {t("btn_join_discord")}
                     </Button>
                 </div>
             </div>
@@ -35,13 +38,14 @@ const FooterSection: React.FC = () => {
                 </div>
 
                 <div className="z-10 max-w-lg text-left text-white">
-                    <div className="font-heading text-2xl">
-                        “TRUE EDUCATION IS BORN WHERE{" "}
+                    <div className="font-heading text-2xl uppercase">
+                        “{t("education_quote_1")}{" "}
                         <span className="relative inline-block">
-                            KNOWLEDGE MEETS ACCESSIBILITY
+                            {t("education_quote_1_hl")}
                             <div className="absolute right-0 top-0 h-full w-0 animate-highlight [background:linear-gradient(55.37deg,_rgba(136,_36,_220,_0.25),_rgba(177,_33,_97,_0.25))]"></div>
-                        </span>{" "}
-                        SESA’S ACADEMIC TEAM IS PROUD TO STAND AT THAT INTERSECTION.”
+                        </span>
+                        {". "}
+                        {t("education_quote_2")}.”
                     </div>
                     <div className="my-3 flex items-center gap-4">
                         <Image
@@ -53,7 +57,7 @@ const FooterSection: React.FC = () => {
                         />
                         <div>
                             <p className="z-20 mt-4 font-heading text-xl">ANGE EMMANUEL</p>
-                            <p className="text-sm opacity-70">Academic Lead</p>
+                            <p className="text-sm opacity-70">{t("academic_lead")}</p>
                         </div>
                     </div>
                     {/* Call To Action Button */}
@@ -62,7 +66,7 @@ const FooterSection: React.FC = () => {
                             href="/pages/TeamPage"
                             className="relative z-10 font-heading text-lg uppercase"
                         >
-                            Join Our Team
+                            {t("btn_join_team")}
                         </Button>
                         <div className="mt-6 flex items-center gap-2">
                             <Image
@@ -72,8 +76,9 @@ const FooterSection: React.FC = () => {
                                 height={20}
                                 className="h-5 w-5"
                             />
-                            <div className="font-[Monocode] leading-[100%] text-thistle">
-                                95% average helpfulness, 1000+ students helped
+                            <div className="font-mono leading-[100%] text-thistle">
+                                {t("avg_helpfulness", { percentage: "95" })},{" "}
+                                {t("students_helped", { students: "1000+" })}
                             </div>
                         </div>
                     </div>
