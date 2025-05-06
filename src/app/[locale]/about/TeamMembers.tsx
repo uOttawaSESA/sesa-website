596bbe45ff733b46d9f5b36f3bad8982b018509c
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-05-05 20:21:42 -0400

    style: Run Prettier

--- src/app/[locale]/about/TeamMembers.tsx
+++ src/app/[locale]/about/TeamMembers.tsx
@@ -72,7 +72,7 @@ export default function TeamMembers({ title, description, people }: Props) {
             <h2 className="font-heading text-2xl uppercase">{title}</h2>
             <p className="max-w-md text-lg leading-tight text-gray-400">{description} </p>
             <div className="flex gap-2">
-                {people.map((person) => (
+                {people.map(person => (
                     <div className="outline-gradient w-64" key={person.name}>
                         <Image
                             src={person.imgPath}
