1b3a663457003be1edafc64b5379aac489f572e1
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-05-09 20:37:59 -0400

    feat(about): Add "Beyond SESA" + various styling tweaks

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -62,7 +62,7 @@ export default function AboutPage() {
                                 key={i}
                                 alt="Profile picture"
                                 size={56}
-                                className={(i & 1) == 1 ? "-m-5" : undefined}
+                                className={(i & 1) === 1 ? "-m-5" : undefined}
                             />
                         ))}
                     </div>
@@ -84,7 +84,7 @@ export default function AboutPage() {
                         We&apos;re a student-led organization made up of{" "}
                         <span className="relative inline-block">
                             nerdy tech geeks
-                            <div className="highlight-gradient"></div>
+                            <div className="highlight-gradient" />
                         </span>
                     </h1>
                     <p className="text-gray-400">
@@ -118,7 +118,7 @@ export default function AboutPage() {
                         Where{" "}
                         <span className="relative inline-block">
                             students meet industry
-                            <div className="highlight-gradient"></div>
+                            <div className="highlight-gradient" />
                         </span>
                     </h1>
                     <div className="max-w-[28rem]">
@@ -129,30 +129,28 @@ export default function AboutPage() {
                         </p>
                     </div>
                     {/* TODO: replace this with a <Button> when it supports localized links */}
-                    <Link
-                        href="/sponsor-us"
-                        className="fill-gradient w-fit px-6 py-3 font-heading uppercase transition-all ease-in-out"
-                    >
+                    <Button href="/sponsor-us" className="w-fit font-heading uppercase">
                         Become a Sponsor
-                    </Link>
+                    </Button>
                 </div>
                 {/* TODO: insert cards */}
                 {/* Introducing our team */}
-                <div className="align-center flex flex-col items-center text-center">
+                <div className="align-center flex flex-col items-center gap-2 text-center">
                     <p className="color-gradient font-mono">Introducing our team</p>
                     <h1 className="font-heading text-3xl uppercase">
                         <span className="relative inline-block">
                             Meet the people
-                            <div className="highlight-gradient"></div>
+                            <div className="highlight-gradient" />
                         </span>{" "}
                         who make SESA
                     </h1>
-                    <p className="my-1 max-w-[32rem] text-lg leading-tight text-gray-400">
+                    <p className="max-w-[32rem] text-lg leading-tight text-gray-400">
                         Meet the incredible back-end team that drives our front-end success and
                         makes everything possible.
                     </p>
                 </div>
-                <div className="sticky top-[5.6rem] z-10 mt-4 flex items-center justify-center font-heading uppercase backdrop-blur-sm">
+                {/* TODO: Add the `sticky` class once a way to make it not super ugly is found */}
+                <div className="top-[5.6rem] z-10 mt-4 flex items-center justify-center font-heading uppercase backdrop-blur-sm">
                     <Button variant="outline" href="#co-directors">
                         Co-directors
                     </Button>
@@ -176,36 +174,74 @@ export default function AboutPage() {
                     </Button>
                 </div>
                 <br />
-                <TeamMembers
-                    title="Co-Directors"
-                    description="Meet our Co-Directors, who keep everything running smoothly. They oversee projects, support the team, and make sure goals are met with a clear vision and strong collaboration."
-                    people={codirectors}
-                />
-                <TeamMembers
-                    title="Partnerships"
-                    description="Relationship builders, collaborating with sponsors, partners, and stakeholders to drive mutual growth and opportunities."
-                    people={partnership}
-                />
-                <TeamMembers
-                    title="Events"
-                    description="The organizers and planners, bringing our community together through impactful workshops, seminars, and conferences."
-                    people={events}
-                />
-                <TeamMembers
-                    title="Communications"
-                    description="Creative minds driving social media, website design, and digital outreach with style and impact."
-                    people={communications}
-                />
-                <TeamMembers
-                    title="Development"
-                    description="The tech powerhouse behind our projects, building innovative solutions and maintaining our website."
-                    people={development}
-                />
-                <TeamMembers
-                    title="Academic"
-                    description="Experts in research and knowledge sharing, supporting educational initiatives and ensuring the quality of academic outputs."
-                    people={academic}
-                />
+                {/* TODO: Add horizontal scrolling if the members don't all fit onscreen. */}
+                <div className="flex flex-col gap-8">
+                    <TeamMembers
+                        title="Co-Directors"
+                        description="Meet our Co-Directors, who keep everything running smoothly. They oversee projects, support the team, and make sure goals are met with a clear vision and strong collaboration."
+                        people={codirectors}
+                    />
+                    <TeamMembers
+                        title="Partnerships"
+                        description="Relationship builders, collaborating with sponsors, partners, and stakeholders to drive mutual growth and opportunities."
+                        people={partnership}
+                    />
+                    <TeamMembers
+                        title="Events"
+                        description="The organizers and planners, bringing our community together through impactful workshops, seminars, and conferences."
+                        people={events}
+                    />
+                    <TeamMembers
+                        title="Communications"
+                        description="Creative minds driving social media, website design, and digital outreach with style and impact."
+                        people={communications}
+                    />
+                    <TeamMembers
+                        title="Development"
+                        description="The tech powerhouse behind our projects, building innovative solutions and maintaining our website."
+                        people={development}
+                    />
+                    <TeamMembers
+                        title="Academic"
+                        description="Experts in research and knowledge sharing, supporting educational initiatives and ensuring the quality of academic outputs."
+                        people={academic}
+                    />
+                </div>
+            </div>
+            {/* Beyond SESA */}
+            <div className="align-center flex flex-col items-center gap-2 text-center">
+                <p className="color-gradient font-mono">Our previous partners</p>
+                <h1 className="font-heading text-3xl uppercase">
+                    <span className="relative inline-block">
+                        Beyond SESA
+                        <div className="highlight-gradient" />
+                    </span>
+                </h1>
+                <p className="max-w-[32rem] text-lg leading-tight text-gray-400">
+                    <b>Our community of past executive members spans around the globe</b>, making
+                    their mark in top industries. Here are just a few places where you'll find the
+                    impact of our talented network driving innovation and success.
+                </p>
+                <div className="flex items-center justify-center text-center">
+                    <div className="grid grid-cols-4 gap-8">
+                        {Array(8)
+                            .fill(0)
+                            .map((_, i) => (
+                                <div
+                                    className="outline-gradient flex h-36 w-64 items-center justify-center"
+                                    key={i}
+                                >
+                                    <Image
+                                        src="/sponsors/warp.svg"
+                                        width={192}
+                                        height={75}
+                                        alt="Warp Logo"
+                                        className="w-48"
+                                    />
+                                </div>
+                            ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
