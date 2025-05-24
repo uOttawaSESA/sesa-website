36a0c7ecf5031e96d5edece4c262b8a7d58c562a
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-05-24 16:23:57 -0400

    feat(about): Prepare page for i18n

--- src/app/[locale]/AboutPage/page.tsx
+++ src/app/[locale]/AboutPage/page.tsx
@@ -6,6 +6,7 @@ import TeamMembers from "./TeamMembers";
 import { useMemo } from "react";
 import { membersData } from "@/app/data/Members";
 import WhatWeDoCard from "./WhatWeDoCard";
+import { useTranslations } from "next-intl";
 
 export default function AboutPage() {
     const images = [
@@ -34,26 +35,26 @@ export default function AboutPage() {
     const events = useMemo(() => membersData.filter(member => member.team === "Events"), []);
     const academic = useMemo(() => membersData.filter(member => member.team === "Academic"), []);
 
+    const t = useTranslations("about");
+    const tWhatWeDo = useTranslations("about.what_do_we_do_cards");
+    const tOurTeam = useTranslations("about.introducing_our_team_section");
+
     return (
         <div className="min-h-screen text-white">
             <div className="container relative mx-auto max-w-7xl px-4 py-8">
                 {/* Upper area */}
                 <div className="flex flex-col gap-2">
                     <div className="flex items-center gap-2 font-heading text-lg uppercase">
-                        <p className="fill-gradient px-2 py-0.5">Partner</p>
-                        <p>Faculty of Engineering</p>
+                        <p className="fill-gradient px-2 py-0.5">{t("partner")}</p>
+                        <p>{t("faculty_of_eng")}</p>
                     </div>
                     <h1 className="max-w-96 font-heading text-4xl uppercase">
-                        Inspiring Future Software Engineers Since 2014
+                        {t("inspiring_heading")}
                     </h1>
-                    <p className="max-w-[28rem] text-gray-400">
-                        We’re a student organization dedicated to enriching SEG student life,
-                        promoting software engineering, and supporting academic and professional
-                        development.
-                    </p>
+                    <p className="max-w-[28rem] text-gray-400">{t("inspiring_blurb")}</p>
                     <div className="my-2 flex gap-2 font-heading uppercase">
                         <Button href="#introducing-our-team" className="font-heading uppercase">
-                            Meet The Team
+                            {t("meet_the_team")}
                         </Button>
                         <Button
                             href="https://linktr.ee/uottawa.sesa"
@@ -61,7 +62,7 @@ export default function AboutPage() {
                             className="font-heading uppercase"
                             variant="outline"
                         >
-                            Join Our Family
+                            {t("join_our_family")}
                         </Button>
                     </div>
                     <div className="flex flex-nowrap items-center">
@@ -88,59 +89,63 @@ export default function AboutPage() {
                 </div>
                 {/* "Who are we" */}
                 <div className="my-8 flex max-w-[28rem] flex-col gap-2 md:ml-[50%]">
-                    <p className="color-gradient font-mono">Who are we?</p>
+                    <p className="color-gradient font-mono">{t("who_are_we")}</p>
                     <h1 className="font-heading text-3xl uppercase">
-                        We&apos;re a student-led organization made up of{" "}
+                        {t("who_are_we_heading")}{" "}
                         <span className="relative inline-block">
-                            nerdy tech geeks
+                            {t("who_are_we_heading_hl")}
                             <div className="highlight-gradient" />
                         </span>
                     </h1>
                     <p className="text-gray-400">
-                        <b>Founded in 2014</b>, the Software Engineering Student Association (SESA)
-                        helps students gain practical, hands-on experience, and make connections
-                        while still in university.
+                        <b>{t("who_are_we_p1_bold")}</b>, {t("who_are_we_p1")}{" "}
                     </p>
                     <p className="my-2 text-gray-400">
-                        <b>Partnered with the Faculty of Engineering, we address challenges</b> in
-                        academic support, professional development, and engagement with the software
-                        engineering industry.
+                        <b>{t("who_are_we_p2_bold")}</b> {t("who_are_we_p2")}
                     </p>
                     <Button href="#introducing-our-team" className="w-fit font-heading uppercase">
-                        Meet The Team
+                        {t("meet_the_team")}
                     </Button>
                 </div>
                 {/* Figures */}
                 <div className="mx-16 flex items-center justify-between">
-                    <Metric className="!w-64" figure="3500+" caption="EECS students" border />
-                    <Metric className="!w-64" figure="21" caption="Previous partners" border />
+                    <Metric
+                        className="!w-64"
+                        figure="3500+"
+                        caption={t("figure_eecs_students")}
+                        border
+                    />
+                    <Metric
+                        className="!w-64"
+                        figure="21"
+                        caption={t("figure_previous_partners")}
+                        border
+                    />
                     <Metric
                         className="!w-64"
                         figure="2300+"
-                        caption="Total event attendees"
+                        caption={t("figure_event_attendees")}
                         border
                     />
-                    <Metric className="!w-64" figure="34" caption="Total events" border />
+                    <Metric className="!w-64" figure="34" caption={t("figure_events")} border />
                 </div>
                 {/* "What do we do" */}
                 <div className="my-8 flex flex-col gap-2">
-                    <p className="color-gradient font-mono">What do we do?</p>
+                    <p className="color-gradient font-mono">{t("what_do_we_do")}</p>
                     <h1 className="font-heading text-3xl uppercase">
-                        Where{" "}
+                        {t("what_do_we_do_heading")}{" "}
                         <span className="relative inline-block">
-                            students meet industry
+                            {t("what_do_we_do_heading_hl")}
                             <div className="highlight-gradient" />
                         </span>
                     </h1>
                     <div className="max-w-[28rem]">
                         <p className="text-gray-400">
-                            <b>We&apos;re preparing the next generation</b> of software engineers
-                            for the workplace by offering exciting experiences and opportunities in
-                            partnership with industry professionals.
+                            <b>{t("what_do_we_do_p_bold")}</b> {t("what_do_we_do_p")}
                         </p>
                     </div>
                     <Button href="/SponsorsPage" className="w-fit font-heading uppercase">
-                        Become a Sponsor &gt;
+                        {t("become_a_sponsor")} &gt;
                     </Button>
                 </div>
                 {/* Cards for "What do we do" */}
@@ -155,9 +160,9 @@ export default function AboutPage() {
                                 alt="Rocket icon"
                             />
                         }
-                        heading="Social Events"
-                        description="We organize educational events and entertaining events, such as movie nights, dog therapy, game nights, and parties."
-                        linkLabel="Discover Events"
+                        heading={tWhatWeDo("social_events_heading")}
+                        description={tWhatWeDo("social_events_desc")}
+                        linkLabel={tWhatWeDo("social_events_btn")}
                         linkHref="/events"
                     />
                     <WhatWeDoCard
@@ -170,9 +175,9 @@ export default function AboutPage() {
                                 alt="School icon"
                             />
                         }
-                        heading="Academic Support"
-                        description="We also provide academic help, including mentorship, resources, advice, and support on coursework, projects, and exams."
-                        linkLabel="Discover Resources"
+                        heading={tWhatWeDo("academic_support_heading")}
+                        description={tWhatWeDo("academic_support_desc")}
+                        linkLabel={tWhatWeDo("academic_support_btn")}
                         linkHref="/resources"
                     />
                     <WhatWeDoCard
@@ -185,9 +190,9 @@ export default function AboutPage() {
                                 alt="Briefcase icon"
                             />
                         }
-                        heading="Professional Development"
-                        description="Additionally, we provide career development opportunities, including workshops, speaker series, and networking events."
-                        linkLabel="Discover Opportunities"
+                        heading={tWhatWeDo("professional_development_heading")}
+                        description={tWhatWeDo("professional_development_desc")}
+                        linkLabel={tWhatWeDo("professional_development_btn")}
                         linkHref="/placeholder"
                     />
                 </div>
@@ -196,91 +201,88 @@ export default function AboutPage() {
                     id="introducing-our-team"
                     className="align-center flex scroll-mt-28 flex-col items-center gap-2 text-center"
                 >
-                    <p className="color-gradient font-mono">Introducing our team</p>
+                    <p className="color-gradient font-mono">{t("introducing_our_team")}</p>
                     <h1 className="font-heading text-3xl uppercase">
                         <span className="relative inline-block">
-                            Meet the people
+                            {t("introducing_our_team_heading_hl")}
                             <div className="highlight-gradient" />
                         </span>{" "}
-                        who make SESA
+                        {t("introducing_our_team_heading")}
                     </h1>
                     <p className="max-w-[32rem] text-lg leading-tight text-gray-400">
-                        Meet the incredible back-end team that drives our front-end success and
-                        makes everything possible.
+                        {t("introducing_our_team_blurb")}
                     </p>
                 </div>
                 {/* TODO: Add the `sticky` class once a way to make it not super ugly is found */}
                 <div className="top-[5.6rem] z-10 mt-4 flex items-center justify-center font-heading uppercase backdrop-blur-sm">
                     <Button variant="outline" href="#co-directors">
-                        Co-directors
+                        {tOurTeam("codirectors")}
                     </Button>
                     <Button variant="outline" href="#partnerships">
-                        Partnerships
+                        {tOurTeam("partnerships")}
                     </Button>
                     <Button variant="outline" href="#events">
-                        Events
+                        {tOurTeam("events")}
                     </Button>
                     <Button variant="outline" href="#communications">
-                        Communications
+                        {tOurTeam("communications")}
                     </Button>
                     <Button variant="outline" href="#development">
-                        Development
+                        {tOurTeam("development")}
                     </Button>
                     <Button variant="outline" href="#academic">
-                        Academic
+                        {tOurTeam("academic")}
                     </Button>
                     <Button variant="outline" href="#advisors">
-                        Advisors
+                        {tOurTeam("advisors")}
                     </Button>
                 </div>
                 <br />
                 {/* TODO: Add horizontal scrolling if the members don't all fit onscreen. */}
                 <div className="flex flex-col gap-8">
                     <TeamMembers
-                        title="Co-Directors"
-                        description="Meet our Co-Directors, who keep everything running smoothly. They oversee projects, support the team, and make sure goals are met with a clear vision and strong collaboration."
+                        title={tOurTeam("codirectors")}
+                        description={tOurTeam("codirectors_desc")}
                         people={codirectors}
                     />
                     <TeamMembers
-                        title="Partnerships"
-                        description="Relationship builders, collaborating with sponsors, partners, and stakeholders to drive mutual growth and opportunities."
+                        title={tOurTeam("partnerships")}
+                        description={tOurTeam("partnerships_desc")}
                         people={partnership}
                     />
                     <TeamMembers
-                        title="Events"
-                        description="The organizers and planners, bringing our community together through impactful workshops, seminars, and conferences."
+                        title={tOurTeam("events")}
+                        description={tOurTeam("events_desc")}
                         people={events}
                     />
                     <TeamMembers
-                        title="Communications"
-                        description="Creative minds driving social media, website design, and digital outreach with style and impact."
+                        title={tOurTeam("communications")}
+                        description={tOurTeam("communications_desc")}
                         people={communications}
                     />
                     <TeamMembers
-                        title="Development"
-                        description="The tech powerhouse behind our projects, building innovative solutions and maintaining our website."
+                        title={tOurTeam("development")}
+                        description={tOurTeam("development_desc")}
                         people={development}
                     />
                     <TeamMembers
-                        title="Academic"
-                        description="Experts in research and knowledge sharing, supporting educational initiatives and ensuring the quality of academic outputs."
+                        title={tOurTeam("academic")}
+                        description={tOurTeam("academic_desc")}
                         people={academic}
                     />
                 </div>
             </div>
             {/* Beyond SESA */}
             <div className="align-center flex flex-col items-center gap-2 text-center">
-                <p className="color-gradient font-mono">Our previous partners</p>
+                <p className="color-gradient font-mono">{t("our_previous_partners")}</p>
                 <h1 className="font-heading text-3xl uppercase">
                     <span className="relative inline-block">
-                        Beyond SESA
+                        {t("our_previous_partners_heading_hl")}
                         <div className="highlight-gradient" />
                     </span>
                 </h1>
                 <p className="max-w-[32rem] text-lg leading-tight text-gray-400">
-                    <b>Our community of past executive members spans around the globe</b>, making
-                    their mark in top industries. Here are just a few places where you&apos;ll find
-                    the impact of our talented network driving innovation and success.
+                    <b>{t("our_previous_partners_p_bold")}</b>, {t("our_previous_partners_p")}{" "}
                 </p>
                 <div className="flex items-center justify-center text-center">
                     <div className="grid grid-cols-4 gap-8">
@@ -307,30 +309,25 @@ export default function AboutPage() {
             {/* Be a part of our family */}
             <div className="mb-24 mt-32 flex items-center justify-center gap-8">
                 <div className="flex flex-col gap-2">
-                    <p className="color-gradient font-mono">Be a part of our family</p>
+                    <p className="color-gradient font-mono">{t("our_family")}</p>
                     <h1 className="max-w-[28rem] font-heading text-3xl uppercase">
-                        Are you passionate about{" "}
+                        {t("our_family_heading")}{" "}
                         <span className="relative inline-block">
-                            Software Engineering?
+                            {t("our_family_heading_hl")}
                             <div className="highlight-gradient" />
                         </span>
                     </h1>
                     <p className="max-w-[28rem] text-lg leading-tight text-gray-400">
-                        <b>Turn your passion into impact</b>. Build, learn, and connect with
-                        like-minded peers while helping shape the next generation of software
-                        engineers.
+                        <b>{t("our_family_p_bold")}</b>. {t("our_family_p")}
                     </p>
                     <Button
                         href="/ContactUsPage"
                         className="my-4 w-fit font-heading uppercase"
                         disabled
                     >
-                        Apply Now
+                        {t("apply_now")}
                     </Button>
-                    <p className="max-w-[28rem] font-mono text-gray-400">
-                        We&apos;re no longer accepting applications for Winter 2025. Stay tuned for
-                        Spring/Summer 2025 openings!
-                    </p>
+                    <p className="max-w-[28rem] font-mono text-gray-400">{t("no_more_apps")}</p>
                 </div>
                 <Image
                     className="outline-gradient h-[500px] w-[700px] object-cover"
