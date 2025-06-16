cb5a3ee677f9c0022f4820063760c1c5d5780ab7
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-16 13:43:27 -0400

    fix: events section font sizing

--- src/app/[locale]/HomeComponents/EventsSection/Events.tsx
+++ src/app/[locale]/HomeComponents/EventsSection/Events.tsx
@@ -17,20 +17,20 @@ export default function Events() {
 
     return (
         <>
-            <section className="relative mt-20 w-full items-center justify-between text-white md:ps-32 2xl:ps-96">
+            <section className="relative mt-4 w-full items-center justify-between p-4 text-white md:mt-20 md:ps-32 2xl:ps-96">
                 {/* Content Container */}
                 <div className="relative z-10 max-w-2xl">
                     <p className="font-monocode relative inline-block !bg-clip-text text-left text-base text-transparent [-webkit-background-clip:text] [-webkit-text-fill-color:transparent] [background:linear-gradient(55.37deg,_#8824dc,_#b1219d)]">
                         {t("events")}
                     </p>
-                    <h1 className="mt-4 font-heading text-4xl uppercase leading-tight">
+                    <h1 className="d:text-5xl mt-4 font-heading text-3xl uppercase leading-tight">
                         <span className="relative inline-block">
                             {t("events_heading_hl")}
                             <div className="absolute right-0 top-0 h-full w-0 animate-highlight [background:linear-gradient(55.37deg,_rgba(136,_36,_220,_0.25),_rgba(177,_33,_97,_0.25))]"></div>
                         </span>{" "}
                         {t("events_heading")}
                     </h1>
-                    <p className="relative flex w-[40rem] items-center text-left font-sans text-xl text-thistle">
+                    <p className="text-md relative flex max-w-72 items-center text-left font-sans text-thistle md:max-w-[40rem] md:text-xl">
                         {t("events_subheading")}
                     </p>
 
