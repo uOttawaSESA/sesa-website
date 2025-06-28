78bcbdcc748791b840981203071be6120f914a02
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-28 10:23:16 -0400

    feat: make team section mobile responsive

--- src/app/[locale]/HomeComponents/TeamSection/Team.tsx
+++ src/app/[locale]/HomeComponents/TeamSection/Team.tsx
@@ -11,34 +11,34 @@ const Team = () => {
 
     return (
         <>
-            <section className="mb-28">
-                <div className="mb-14 flex flex-col items-center justify-center gap-2">
-                    <div className="mb-4 flex flex-row gap-3">
+            <section className="flex flex-col gap-4">
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <div className="flex flex-row gap-3">
                         <Image src="/logo-filled.svg" width={50} height={50} alt="sesa" />
                         <Image src="/logo-text.svg" width={111} height={50} alt="sesa" />
                     </div>
-                    <h1 className="w-[47rem] text-center font-heading text-4xl uppercase leading-tight">
-                        The{" "}
-                        <span className="relative inline-block">
+                    <h1 className="text-center font-heading text-3xl uppercase leading-tight md:text-5xl">
+                        The&nbsp;
+                        <span className="relative inline-block w-max">
                             Univeristy of Ottawa&apos;s
                             <div className="absolute right-0 top-0 h-full w-0 animate-highlight [background:linear-gradient(55.37deg,_rgba(136,_36,_220,_0.25),_rgba(177,_33,_97,_0.25))]"></div>
-                        </span>{" "}
-                        Software Engineering Students Association
+                        </span>
+                        &nbsp; Software Engineering Students Association
                     </h1>
-                    <p className="mt-2 px-96 text-center font-sans text-xl text-thistle">
+                    <p className="text-center font-sans text-base text-thistle md:text-xl">
                         Our communications team meticulously crafted the web design, while our
                         development team built it with heart.
                     </p>
                 </div>
 
-                <div className="mt-8 flex justify-center">
-                    <div className="flex space-x-4 font-heading">
-                        <Button className="font-heading text-lg uppercase" href="#">
+                <div className="flex justify-center">
+                    <div className="flex gap-4 font-heading">
+                        <Button className="w-max font-heading text-lg uppercase" href="#">
                             Become a sponsor <span className="text-gray opacity-50">{">"}</span>
                         </Button>
 
                         <Button
-                            className="font-heading text-lg uppercase"
+                            className="w-max font-heading text-lg uppercase"
                             variant="outline"
                             href="/ContactUsPage"
                         >
@@ -47,7 +47,7 @@ const Team = () => {
                     </div>
                 </div>
 
-                <div className="relative mt-12 bg-transparent">
+                <div className="relative bg-transparent">
                     <Marquee pauseOnHover speed={40}>
                         <div className="flex flex-row py-5">
                             {membersData.map((member, index) => (
