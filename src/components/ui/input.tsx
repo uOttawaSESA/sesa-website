32f670c7829b9ef73b9e2a466764f5eb71892d4f
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-07-26 20:35:02 -0400

    feat(ui): Customize input and textarea
    Applies SESA styling to the shadcn/ui components.

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
             <input
                 type={type}
                 className={cn(
-                    "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+                    "outline-gradient relative min-h-[3.5rem] w-full rounded-none bg-[rgba(27,27,27,0.05)] px-3 py-2 font-sans text-[#AB9DB6] shadow-sm transition-[border-width,box-shadow] duration-150 ease-in-out file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-thistle focus:border-[3px] focus:shadow-[0_0_4px_rgba(255,255,255,0.4)] focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
                     className,
                 )}
                 ref={ref}
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -7,7 +7,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<"tex
         return (
             <textarea
                 className={cn(
-                    "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+                    "outline-gradient relative flex min-h-[60px] w-full rounded-none bg-[rgba(27,27,27,0.05)] px-3 py-2 font-sans text-[#AB9DB6] shadow-sm transition-[border-width,box-shadow] duration-150 ease-in-out placeholder:text-thistle focus:border-[3px] focus:shadow-[0_0_4px_rgba(255,255,255,0.4)] focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
                     className,
                 )}
                 ref={ref}
