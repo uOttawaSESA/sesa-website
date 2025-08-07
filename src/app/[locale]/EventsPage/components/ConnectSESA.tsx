uOttawaSESA/sesa-website

commit e9a942d226024b6a7402998a5c0e80bfd684504a

Remake footer for new design (#10)
* feat: Remake footer for new design

* feat: Add "Back to top" link to footer

* style: Run Prettier

* feat: Add social media icon SVGs

* feat: Update footer to use actual social media icons

* feat: Add social media links to footer

Note about the Discord invite: I just checked the "Invites" tab on the
server and used whichever link had the most invites. This can be changed
later if we want to use a site-specific link to track joins.

* feat: Use unique Discord invite link

* feat: Left-align footer except on small screens

* feat: Add sponsor carousel

* feat: Make sponsors hyperlinks + add new class

* feat: Add SESA logo + text to footer

* feat: Make social media links open in new tab

* feat: Comment out sponsor carousel

Code can be reused if we want to put it elsewhere.

* fix: Correct font for footer links

* feat: Extend gradient to footer

src/components/Footer.tsx
@@ -1,107 +1,165 @@
 import Image from "next/image";
-import IconButton from "./IconButton";
+import Button from "./Button";
+
+// === Removed sponsor marquee, in case we want it again: ===
+// {/* Sponsors marquee */}
+// {/* Implementation detail: SVG's are inlined so that the colour may be changed based on browser theme */}
+// <Marquee pauseOnHover autoFill>
+//     <div className="sponsor">
+//         <a href="https://www.warp.dev/" target="_blank">
+//             {/* prettier-ignore */}
+//             <svg fill="none" height="36" viewBox="0 0 380 92" width="150" xmlns="http://www.w3.org/2000/svg"><path d="M51.0696 4.92194H88.7341C94.8309 4.92194 99.7736 10.0293 99.7736 16.3295V60.6175C99.7736 66.9179 94.8309 72.0252 88.7341 72.0252H34.835L51.0696 4.92194Z" fill="currentColor"></path><path d="M41.2866 17.6346H10.9455C4.90046 17.6346 0 22.7419 0 29.0421V73.3302C0 79.6305 4.90046 84.7378 10.9455 84.7378H48.2888L49.7863 78.495H26.6878L41.2866 17.6346Z" fill="currentColor"></path><path d="M145.001 71.9982L128.396 17.6544H137.777L149.53 59.167L162.253 17.6544H171.095L183.818 59.167L195.463 17.6544H205.06L188.347 71.9982H179.397L166.674 30.7013L153.95 71.9982H145.001ZM246.426 36.6316C246.426 29.1917 241.142 24.4474 233.163 24.4474C225.723 24.4474 220.44 28.8682 219.685 36.2003L211.167 34.583C212.784 23.8005 221.734 16.7918 233.163 16.7918C246.318 16.7918 255.483 24.5552 255.483 37.1708V71.9982H246.426V62.7253C243.299 68.9791 236.182 72.8608 228.311 72.8608C217.421 72.8608 209.981 66.1757 209.981 56.7949C209.981 46.3359 218.499 40.6211 235.859 38.896L246.426 37.7099V36.6316ZM219.146 56.6871C219.146 61.7548 223.459 65.3131 229.713 65.3131C240.28 65.3131 246.426 58.3044 246.426 47.4141V44.8263L235.859 46.0124C224.861 47.1985 219.146 50.9723 219.146 56.6871ZM317.181 36.0925L308.016 37.8177C307.584 29.7308 302.517 25.0944 294.322 25.0944C284.294 25.0944 277.825 34.1517 277.825 48.708V71.9982H268.66V17.6544H277.825V28.3291C281.814 20.5657 288.499 16.7918 296.478 16.7918C308.555 16.7918 316.75 24.3396 317.181 36.0925ZM326.95 91.6223V17.6544H336.115V25.5257C339.027 20.5657 346.143 16.7918 354.014 16.7918C370.727 16.7918 380 28.8682 380 44.8263C380 60.7844 370.512 72.8608 353.799 72.8608C346.898 72.8608 339.781 69.4104 336.115 64.4505V91.6223H326.95ZM352.936 64.6661C363.719 64.6661 370.727 56.7949 370.727 44.8263C370.727 32.8578 363.719 24.9865 352.936 24.9865C342.369 24.9865 335.361 32.8578 335.361 44.8263C335.361 56.7949 342.369 64.6661 352.936 64.6661Z" fill="currentColor"></path></svg>
+//         </a>
+//     </div>
+//     <div className="sponsor">
+//         <p className="text-4xl">SPONSOR</p>
+//     </div>
+// </Marquee>
 
 export default function Footer() {
     return (
-        <footer className="bg-gray mx-8 my-8 xl:mx-32 2xl:mx-64">
-            <div className="flex flex-col justify-between [backdrop-filter:blur(100px)] lg:flex-row">
-                <div className="flex items-center justify-center gap-4">
-                    <Image
-                        className="my-0"
-                        width="50"
-                        height="50"
-                        src="sesa-logo.svg"
-                        alt="SESA Logo"
-                    />
-                    <Image
-                        className="my-0"
-                        width="109"
-                        height="25"
-                        src="logo-text.svg"
-                        alt="SESA Logo Text"
-                    />
-                </div>
-                <div className="flex flex-col items-center justify-center md:flex-row md:gap-12">
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Team
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Events
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Resources
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Contact
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Sponsor
-                    </a>
-                    <a className="my-4 font-heading text-xl uppercase" href="#">
-                        Privacy &amp; Terms
-                    </a>
-                </div>
+        <footer className="mx-0 flex flex-col gap-2 bg-transparent px-0 pt-8 backdrop-blur-[50px]">
+            {/* SESA logo + text */}
+            <div className="mx-8 flex justify-center gap-4 sm:justify-start xl:mx-32 2xl:mx-64">
+                <Image
+                    className="my-0"
+                    width="50"
+                    height="50"
+                    src="sesa-logo.svg"
+                    alt="SESA Logo"
+                />
+                <Image
+                    className="my-0"
+                    width="109"
+                    height="25"
+                    src="logo-text.svg"
+                    alt="SESA Logo Text"
+                />
             </div>
-            <div className="flex items-center justify-between">
-                <p>
-                    &copy; 2014&ndash;{new Date().getFullYear()} uOttawa Software Engineering
-                    Student Association (SESA)
-                </p>
-                <div className="flex gap-4">
-                    <IconButton href="#" variant="outline">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </IconButton>
-                    <IconButton href="#" variant="outline">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </IconButton>
-                    <IconButton href="#" variant="outline">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </IconButton>
-                    <IconButton href="#" variant="outline">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </IconButton>
-                    <IconButton href="#" variant="outline">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </IconButton>
-                    <IconButton href="#" variant="outline">
-                        {" "}
-                        <Image
-                            width="25"
-                            height="25"
-                            src="sesa-logo.svg"
-                            alt="Instagram Logo"
-                        />{" "}
-                    </IconButton>
+            {/* Navigation links and socials */}
+            <div className="mx-8 my-8 flex flex-col gap-4 xl:mx-32 2xl:mx-64">
+                <div className="grid grid-cols-1 gap-4 text-center sm:grid-cols-2 sm:text-left lg:grid-cols-4">
+                    <div className="flex flex-col gap-4">
+                        <h3 className="color-gradient font-mono">Contact</h3>
+                        <p className="font-mono text-gray-400">
+                            800 King Edward Ave,
+                            <br />
+                            Ottawa, ON, K1N 1A2,
+                            <br />
+                            STE 0109
+                            <br />
+                            uottawa.sesa@gmail.com
+                        </p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <h3 className="color-gradient font-mono">Legal</h3>
+                        <ul className="flex flex-col gap-4">
+                            <li>
+                                <a className="font-heading text-xl uppercase" href="#">
+                                    Privacy Policy
+                                </a>
+                            </li>
+                            <li>
+                                <a className="font-heading text-xl uppercase" href="#">
+                                    Terms of Service
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <h3 className="color-gradient font-mono">Navigation</h3>
+                        <ul className="flex flex-col gap-4">
+                            <li>
+                                <a className="font-heading text-xl uppercase" href="#">
+                                    Team
+                                </a>
+                            </li>
+                            <li>
+                                <a className="font-heading text-xl uppercase" href="#">
+                                    Events
+                                </a>
+                            </li>
+                            <li>
+                                <a className="font-heading text-xl uppercase" href="#">
+                                    Resources
+                                </a>
+                            </li>
+                            <li>
+                                <a className="font-heading text-xl uppercase" href="#">
+                                    Contact
+                                </a>
+                            </li>
+                            <li>
+                                <a className="font-heading text-xl uppercase" href="#">
+                                    Sponsor
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <h3 className="color-gradient font-mono">Socials</h3>
+                        <div className="flex justify-center gap-2 sm:justify-start">
+                            <Button
+                                className="!p-3"
+                                variant="outline"
+                                href="https://www.instagram.com/uottawasesa/"
+                                target="_blank"
+                            >
+                                <Image
+                                    src="icons/instagram-plain.svg"
+                                    width={25}
+                                    height={25}
+                                    alt="Instagram Logo"
+                                />
+                            </Button>
+                            <Button
+                                className="!p-3"
+                                variant="outline"
+                                href="https://discord.com/invite/atYdx5HHCs"
+                                target="_blank"
+                            >
+                                <Image
+                                    src="icons/discord-plain.svg"
+                                    width={25}
+                                    height={25}
+                                    alt="Discord Logo"
+                                />
+                            </Button>
+                            <Button
+                                className="!p-3"
+                                variant="outline"
+                                href="https://www.linkedin.com/company/software-engineering-students-association/"
+                                target="_blank"
+                            >
+                                <Image
+                                    src="icons/linkedin-plain.svg"
+                                    width={25}
+                                    height={25}
+                                    alt="LinkedIn Logo"
+                                />
+                            </Button>
+                            <Button
+                                className="!p-3"
+                                variant="outline"
+                                href="https://www.youtube.com/@uottawasesa52"
+                                target="_blank"
+                            >
+                                <Image
+                                    src="icons/youtube-plain.svg"
+                                    width={25}
+                                    height={25}
+                                    alt="YouTube Logo"
+                                />
+                            </Button>
+                        </div>
+                    </div>
+                    <p className="text-left text-gray-400 sm:col-span-2">
+                        &copy; 2014&ndash;{new Date().getFullYear()} uOttawa Software Engineering
+                        Student Association (SESA)
+                    </p>
+                    <a href="#" className="text-right text-gray-400 sm:col-span-2">
+                        Back to top
+                    </a>
                 </div>
             </div>
         </footer>

public/icons/discord-gradient.svg
@@ -1,0 +1,14 @@
+<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="discord 2" clip-path="url(#clip0_567_3867)">
+<path id="Vector" d="M21.1638 4.5422C19.5429 3.7978 17.8323 3.2668 16.0748 2.96251C16.0589 2.95983 16.0425 2.96215 16.0279 2.96914C16.0134 2.97614 16.0014 2.98747 15.9935 3.00157C15.7732 3.3922 15.5295 3.90314 15.3591 4.30314C13.4648 4.01589 11.5379 4.01589 9.64353 4.30314C9.45434 3.85717 9.2394 3.42259 8.99978 3.00157C8.99193 2.98747 8.9799 2.97614 8.96535 2.96914C8.9508 2.96215 8.93445 2.95983 8.91853 2.96251C7.16071 3.26564 5.44978 3.79689 3.82946 4.5422C3.81553 4.54737 3.8039 4.55735 3.79665 4.57032C0.556026 9.41251 -0.333037 14.1359 0.102901 18.8C0.106026 18.8229 0.116963 18.8422 0.135713 18.8578C2.02316 20.255 4.13378 21.3222 6.3779 22.0141C6.39375 22.0191 6.41077 22.0189 6.42651 22.0136C6.44225 22.0082 6.45587 21.998 6.4654 21.9844C6.94769 21.3281 7.37373 20.6359 7.74353 19.9078C7.75121 19.8928 7.75384 19.8757 7.75103 19.8591C7.74821 19.8425 7.7401 19.8273 7.7279 19.8156L7.69978 19.7984C7.0269 19.539 6.37498 19.2282 5.74978 18.8688C5.73232 18.8589 5.71934 18.8426 5.71353 18.8234C5.70771 18.8042 5.7095 18.7836 5.71853 18.7656L5.74196 18.7359C5.87425 18.637 6.00342 18.5354 6.12946 18.4313C6.14051 18.4224 6.15375 18.4167 6.16779 18.4147C6.18183 18.4128 6.19612 18.4147 6.20915 18.4203C10.3013 20.2891 14.731 20.2891 18.7732 18.4203C18.7866 18.4141 18.8015 18.4119 18.8162 18.4138C18.8309 18.4157 18.8447 18.4218 18.856 18.4313C18.9821 18.5354 19.1112 18.637 19.2435 18.7359C19.2545 18.7435 19.2634 18.7537 19.2694 18.7656C19.2753 18.7775 19.2781 18.7908 19.2774 18.8041C19.2768 18.8174 19.2728 18.8303 19.2658 18.8417C19.2587 18.853 19.2489 18.8623 19.2373 18.8688C18.6148 19.2339 17.9618 19.5444 17.2857 19.7969C17.2749 19.8008 17.2652 19.807 17.2571 19.8151C17.249 19.8232 17.2427 19.833 17.2388 19.8438C17.235 19.8542 17.2335 19.8653 17.2343 19.8763C17.2351 19.8873 17.2382 19.8981 17.2435 19.9078C17.6185 20.6344 18.0482 21.3281 18.5201 21.9844C18.5296 21.998 18.5432 22.0082 18.559 22.0136C18.5747 22.0189 18.5917 22.0191 18.6076 22.0141C20.8561 21.326 22.9705 20.2586 24.8591 18.8578C24.8685 18.8512 24.8763 18.8426 24.882 18.8326C24.8877 18.8226 24.8911 18.8115 24.892 18.8C25.4138 13.4078 24.0185 8.72345 21.1951 4.57189C21.1926 4.5648 21.1885 4.5584 21.1831 4.55324C21.1776 4.54807 21.171 4.54429 21.1638 4.5422ZM8.35446 15.9594C7.12165 15.9594 6.10759 14.8281 6.10759 13.4406C6.10759 12.0531 7.1029 10.9203 8.35446 10.9203C9.6154 10.9203 10.6201 12.0609 10.6013 13.4406C10.6013 14.8281 9.60603 15.9594 8.35446 15.9594ZM16.6607 15.9594C15.4295 15.9594 14.4138 14.8281 14.4138 13.4406C14.4138 12.0531 15.4092 10.9203 16.6607 10.9203C17.9216 10.9203 18.9279 12.0609 18.9076 13.4406C18.9076 14.8281 17.9216 15.9594 16.6607 15.9594Z" fill="url(#paint0_linear_567_3867)"/>
+</g>
+<defs>
+<linearGradient id="paint0_linear_567_3867" x1="18.6101" y1="22.9974" x2="27.8841" y2="14.5935" gradientUnits="userSpaceOnUse">
+<stop stop-color="#8824DC"/>
+<stop offset="1" stop-color="#B1219D"/>
+</linearGradient>
+<clipPath id="clip0_567_3867">
+<rect width="25" height="25" fill="white"/>
+</clipPath>
+</defs>
+</svg>

public/icons/discord-plain.svg
@@ -1,0 +1,10 @@
+<svg width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="discord 2" clip-path="url(#clip0_1197_7200)">
+<path id="Vector" d="M21.1643 5.04171C19.5434 4.29731 17.8328 3.76632 16.0753 3.46202C16.0593 3.45934 16.043 3.46166 16.0284 3.46865C16.0139 3.47565 16.0019 3.48698 15.994 3.50109C15.7737 3.89171 15.53 4.40265 15.3596 4.80265C13.4653 4.5154 11.5384 4.5154 9.64401 4.80265C9.45483 4.35669 9.23989 3.9221 9.00026 3.50109C8.99242 3.48698 8.98039 3.47565 8.96584 3.46865C8.95129 3.46166 8.93493 3.45934 8.91901 3.46202C7.1612 3.76515 5.45026 4.2964 3.82995 5.04171C3.81602 5.04689 3.80438 5.05686 3.79714 5.06984C0.556514 9.91202 -0.332549 14.6355 0.103389 19.2995C0.106514 19.3224 0.117451 19.3417 0.136201 19.3573C2.02365 20.7546 4.13427 21.8218 6.37839 22.5136C6.39423 22.5186 6.41126 22.5184 6.427 22.5131C6.44273 22.5077 6.45636 22.4975 6.46589 22.4839C6.94818 21.8276 7.37422 21.1355 7.74401 20.4073C7.7517 20.3923 7.75433 20.3753 7.75152 20.3586C7.7487 20.342 7.74059 20.3268 7.72839 20.3151L7.70026 20.298C7.02739 20.0385 6.37546 19.7277 5.75026 19.3683C5.73281 19.3584 5.71983 19.3422 5.71401 19.323C5.7082 19.3038 5.70999 19.2831 5.71901 19.2651L5.74245 19.2355C5.87474 19.1365 6.00391 19.0349 6.12995 18.9308C6.14099 18.9219 6.15424 18.9162 6.16828 18.9143C6.18232 18.9123 6.19661 18.9143 6.20964 18.9198C10.3018 20.7886 14.7315 20.7886 18.7737 18.9198C18.7871 18.9136 18.802 18.9114 18.8167 18.9133C18.8314 18.9152 18.8452 18.9213 18.8565 18.9308C18.9826 19.0349 19.1117 19.1365 19.244 19.2355C19.255 19.243 19.2639 19.2532 19.2698 19.2651C19.2758 19.2771 19.2786 19.2903 19.2779 19.3036C19.2773 19.3169 19.2733 19.3298 19.2663 19.3412C19.2592 19.3525 19.2494 19.3618 19.2378 19.3683C18.6153 19.7334 17.9623 20.0439 17.2862 20.2964C17.2754 20.3003 17.2656 20.3065 17.2575 20.3146C17.2494 20.3227 17.2432 20.3325 17.2393 20.3433C17.2355 20.3537 17.234 20.3648 17.2348 20.3758C17.2356 20.3869 17.2387 20.3976 17.244 20.4073C17.619 21.1339 18.0487 21.8276 18.5206 22.4839C18.5301 22.4975 18.5437 22.5077 18.5595 22.5131C18.5752 22.5184 18.5922 22.5186 18.6081 22.5136C20.8566 21.8256 22.9709 20.7581 24.8596 19.3573C24.869 19.3507 24.8768 19.3421 24.8825 19.3321C24.8882 19.3221 24.8916 19.311 24.8925 19.2995C25.4143 13.9073 24.019 9.22296 21.1956 5.0714C21.1931 5.06431 21.189 5.05792 21.1836 5.05275C21.1781 5.04758 21.1715 5.0438 21.1643 5.04171ZM8.35495 16.4589C7.12214 16.4589 6.10808 15.3276 6.10808 13.9401C6.10808 12.5526 7.10339 11.4198 8.35495 11.4198C9.61589 11.4198 10.6206 12.5605 10.6018 13.9401C10.6018 15.3276 9.60651 16.4589 8.35495 16.4589ZM16.6612 16.4589C15.43 16.4589 14.4143 15.3276 14.4143 13.9401C14.4143 12.5526 15.4096 11.4198 16.6612 11.4198C17.9221 11.4198 18.9284 12.5605 18.9081 13.9401C18.9081 15.3276 17.9221 16.4589 16.6612 16.4589Z" fill="white"/>
+</g>
+<defs>
+<clipPath id="clip0_1197_7200">
+<rect width="25" height="25" fill="white" transform="translate(0 0.5)"/>
+</clipPath>
+</defs>
+</svg>

public/icons/github-gradient.svg
@@ -1,0 +1,14 @@
+<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="github 1" clip-path="url(#clip0_567_3870)">
+<path id="Vector" d="M12.5 0C5.59375 0 0 5.59375 0 12.5C0 18.0312 3.57812 22.7031 8.54688 24.3594C9.17188 24.4688 9.40625 24.0938 9.40625 23.7656C9.40625 23.4688 9.39062 22.4844 9.39062 21.4375C6.25 22.0156 5.4375 20.6719 5.1875 19.9688C5.04688 19.6094 4.4375 18.5 3.90625 18.2031C3.46875 17.9688 2.84375 17.3906 3.89062 17.375C4.875 17.3594 5.57812 18.2812 5.8125 18.6562C6.9375 20.5469 8.73438 20.0156 9.45312 19.6875C9.5625 18.875 9.89062 18.3281 10.25 18.0156C7.46875 17.7031 4.5625 16.625 4.5625 11.8438C4.5625 10.4844 5.04688 9.35938 5.84375 8.48438C5.71875 8.17188 5.28125 6.89062 5.96875 5.17188C5.96875 5.17188 7.01562 4.84375 9.40625 6.45312C10.4062 6.17188 11.4688 6.03125 12.5313 6.03125C13.5938 6.03125 14.6563 6.17188 15.6563 6.45312C18.0469 4.82812 19.0938 5.17188 19.0938 5.17188C19.7812 6.89062 19.3438 8.17188 19.2188 8.48438C20.0156 9.35938 20.5 10.4688 20.5 11.8438C20.5 16.6406 17.5781 17.7031 14.7969 18.0156C15.25 18.4062 15.6406 19.1562 15.6406 20.3281C15.6406 22 15.625 23.3438 15.625 23.7656C15.625 24.0938 15.8594 24.4844 16.4844 24.3594C18.9662 23.5223 21.1229 21.9276 22.6506 19.8C24.1783 17.6725 25 15.1192 25 12.5C25 5.59375 19.4062 0 12.5 0Z" fill="url(#paint0_linear_567_3870)"/>
+</g>
+<defs>
+<linearGradient id="paint0_linear_567_3870" x1="18.6104" y1="25.6368" x2="29.8583" y2="17.6711" gradientUnits="userSpaceOnUse">
+<stop stop-color="#8824DC"/>
+<stop offset="1" stop-color="#B1219D"/>
+</linearGradient>
+<clipPath id="clip0_567_3870">
+<rect width="25" height="25" fill="white"/>
+</clipPath>
+</defs>
+</svg>

public/icons/instagram-gradient.svg
@@ -1,0 +1,14 @@
+<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="instagram 2" clip-path="url(#clip0_567_3864)">
+<path id="Vector" d="M12.5 0C9.10781 0 8.68125 0.015625 7.34844 0.075C6.01562 0.1375 5.10781 0.346875 4.3125 0.65625C3.47788 0.969259 2.7221 1.46156 2.09844 2.09844C1.46156 2.7221 0.969259 3.47788 0.65625 4.3125C0.346875 5.10625 0.135937 6.01562 0.075 7.34375C0.015625 8.67969 0 9.10469 0 12.5016C0 15.8953 0.015625 16.3203 0.075 17.6531C0.1375 18.9844 0.346875 19.8922 0.65625 20.6875C0.976562 21.5094 1.40313 22.2062 2.09844 22.9016C2.79219 23.5969 3.48906 24.025 4.31094 24.3438C5.10781 24.6531 6.01406 24.8641 7.34531 24.925C8.67969 24.9844 9.10469 25 12.5 25C15.8953 25 16.3188 24.9844 17.6531 24.925C18.9828 24.8625 19.8938 24.6531 20.6891 24.3438C21.5231 24.0305 22.2784 23.5382 22.9016 22.9016C23.5969 22.2062 24.0234 21.5094 24.3438 20.6875C24.6516 19.8922 24.8625 18.9844 24.925 17.6531C24.9844 16.3203 25 15.8953 25 12.5C25 9.10469 24.9844 8.67969 24.925 7.34531C24.8625 6.01563 24.6516 5.10625 24.3438 4.3125C24.0307 3.47788 23.5384 2.7221 22.9016 2.09844C22.2779 1.46156 21.5221 0.969259 20.6875 0.65625C19.8906 0.346875 18.9813 0.135937 17.6516 0.075C16.3172 0.015625 15.8938 0 12.4969 0H12.5ZM11.3797 2.25312H12.5016C15.8391 2.25312 16.2344 2.26406 17.5516 2.325C18.7703 2.37969 19.4328 2.58437 19.8734 2.75469C20.4562 2.98125 20.8734 3.25312 21.3109 3.69062C21.7484 4.12813 22.0188 4.54375 22.2453 5.12813C22.4172 5.56719 22.6203 6.22969 22.675 7.44844C22.7359 8.76562 22.7484 9.16094 22.7484 12.4969C22.7484 15.8328 22.7359 16.2297 22.675 17.5469C22.6203 18.7656 22.4156 19.4266 22.2453 19.8672C22.0435 20.4092 21.7238 20.8997 21.3094 21.3031C20.8719 21.7406 20.4563 22.0109 19.8719 22.2375C19.4344 22.4094 18.7719 22.6125 17.5516 22.6688C16.2344 22.7281 15.8391 22.7422 12.5016 22.7422C9.16406 22.7422 8.76719 22.7281 7.45 22.6688C6.23125 22.6125 5.57031 22.4094 5.12969 22.2375C4.58724 22.0363 4.09625 21.7172 3.69219 21.3031C3.27699 20.8994 2.95676 20.4084 2.75469 19.8656C2.58437 19.4266 2.37969 18.7641 2.325 17.5453C2.26562 16.2281 2.25312 15.8328 2.25312 12.4937C2.25312 9.15469 2.26562 8.7625 2.325 7.44531C2.38125 6.22656 2.58438 5.56406 2.75625 5.12344C2.98281 4.54063 3.25469 4.12344 3.69219 3.68594C4.12969 3.24844 4.54531 2.97813 5.12969 2.75156C5.57031 2.57969 6.23125 2.37656 7.45 2.32031C8.60313 2.26719 9.05 2.25156 11.3797 2.25V2.25312ZM19.1734 4.32812C18.9765 4.32812 18.7814 4.36692 18.5994 4.44231C18.4174 4.51769 18.2521 4.62818 18.1128 4.76746C17.9735 4.90675 17.863 5.07211 17.7876 5.2541C17.7122 5.43609 17.6734 5.63114 17.6734 5.82812C17.6734 6.02511 17.7122 6.22016 17.7876 6.40215C17.863 6.58414 17.9735 6.7495 18.1128 6.88879C18.2521 7.02807 18.4174 7.13856 18.5994 7.21394C18.7814 7.28933 18.9765 7.32812 19.1734 7.32812C19.5713 7.32812 19.9528 7.17009 20.2341 6.88879C20.5154 6.60748 20.6734 6.22595 20.6734 5.82812C20.6734 5.4303 20.5154 5.04877 20.2341 4.76746C19.9528 4.48616 19.5713 4.32812 19.1734 4.32812ZM12.5016 6.08125C11.6501 6.06797 10.8045 6.2242 10.014 6.54085C9.22353 6.85749 8.50393 7.32824 7.89711 7.92566C7.29029 8.52309 6.80838 9.23526 6.47944 10.0207C6.1505 10.8062 5.98109 11.6492 5.98109 12.5008C5.98109 13.3523 6.1505 14.1954 6.47944 14.9808C6.80838 15.7663 7.29029 16.4785 7.89711 17.0759C8.50393 17.6733 9.22353 18.1441 10.014 18.4607C10.8045 18.7774 11.6501 18.9336 12.5016 18.9203C14.1868 18.894 15.7941 18.2061 16.9765 17.0051C18.159 15.804 18.8217 14.1862 18.8217 12.5008C18.8217 10.8154 18.159 9.19752 16.9765 7.99648C15.7941 6.79545 14.1868 6.10754 12.5016 6.08125ZM12.5016 8.33281C13.0488 8.33281 13.5907 8.4406 14.0963 8.65002C14.6019 8.85944 15.0613 9.16639 15.4482 9.55335C15.8352 9.94031 16.1421 10.3997 16.3515 10.9053C16.561 11.4109 16.6688 11.9528 16.6688 12.5C16.6688 13.0472 16.561 13.5891 16.3515 14.0947C16.1421 14.6003 15.8352 15.0597 15.4482 15.4466C15.0613 15.8336 14.6019 16.1406 14.0963 16.35C13.5907 16.5594 13.0488 16.6672 12.5016 16.6672C11.3964 16.6672 10.3364 16.2281 9.55492 15.4466C8.77342 14.6651 8.33438 13.6052 8.33438 12.5C8.33438 11.3948 8.77342 10.3349 9.55492 9.55335C10.3364 8.77185 11.3964 8.33281 12.5016 8.33281Z" fill="url(#paint0_linear_567_3864)"/>
+</g>
+<defs>
+<linearGradient id="paint0_linear_567_3864" x1="18.6104" y1="26.2853" x2="30.0444" y2="18.3875" gradientUnits="userSpaceOnUse">
+<stop stop-color="#8824DC"/>
+<stop offset="1" stop-color="#B1219D"/>
+</linearGradient>
+<clipPath id="clip0_567_3864">
+<rect width="25" height="25" fill="white"/>
+</clipPath>
+</defs>
+</svg>

commit c8e1b80bd4034e6ef6a62c2c2b1a372be3550dbc

fix: Update invalid YouTube links
Fixes: e9a942d (Remake footer for new design (#10))

src/app/[locale]/EventsPage/components/ConnectSESA.tsx
@@ -87,7 +87,7 @@ const ConnectSESA = () => {
                     </Button>
                     <Button size="icon" variant="outline" asChild>
                         <a
-                            href="https://www.youtube.com/@uottawasesa52"
+                            href="https://www.youtube.com/@uottawasesa527"
                             target="_blank"
                             rel="noopener noreferrer"
                         >

src/app/[locale]/HomeComponents/ConnectSection/Connect.tsx
@@ -78,7 +78,7 @@ const Connect = () => {
                                 </a>
                             </Button>
                             <Button size="icon" variant="outline" asChild>
-                                <a href="https://www.youtube.com/@uottawasesa52" target="_blank">
+                                <a href="https://www.youtube.com/@uottawasesa527" target="_blank">
                                     <Image
                                         src="/icons/youtube-plain.svg"
                                         width={25}

src/components/Footer.tsx
@@ -131,7 +131,7 @@ export default function Footer() {
                                 </Button>
                                 <Button size="icon" variant="outline" asChild>
                                     <a
-                                        href="https://www.youtube.com/@uottawasesa52"
+                                        href="https://www.youtube.com/@uottawasesa527"
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >