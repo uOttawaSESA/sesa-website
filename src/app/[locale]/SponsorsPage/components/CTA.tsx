uOttawaSESA/sesa-website

commit 1618a3ea32d134eca8f6faf80100ce1c40bffa06

sponsor page initial commit (#64)
* sponsor page initial commit

* Update resourcesData.tsx

* sponsor img sizes fixed (not responsive yet)

* sponsor page changes

* color fixes

* btn link

* linting issues resolved

---------

Co-authored-by: Asad Ali <masadbali190@gmail.com>

src/app/[locale]/SponsorsPage/components/CTA.tsx
@@ -1,0 +1,24 @@
+"use client";
+import Button from "@/components/Button";
+import Link from "next/link";
+
+export default function CTA() {
+    return (
+        <div className="relative z-10 flex w-full flex-col items-center justify-center px-4 py-8 text-center">
+            <div className="flex flex-wrap items-center justify-center gap-3">
+                <p className="text-m font-sans text-white">
+                    Interested in joining our growing network of industry partners?
+                </p>
+                <Link href="/contact">
+                    <Button className="px-4 py-1.5 font-heading text-sm uppercase text-white">
+                        Let&apos;s Talk
+                    </Button>
+                </Link>
+            </div>
+
+            <p className="mt-4 font-sans text-sm text-thistle">
+                We&apos;ll get back to you within 48 hours.
+            </p>
+        </div>
+    );
+}

commit 57e7ab49e3d2685bb9e4fefa59e33dfc00bfd1ee

fix(sponsor): Fix sponsorship page contact link
Fixes: 1618a3e (sponsor page initial commit (#64))

src/app/[locale]/SponsorsPage/components/CTA.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Link } from "@/i18n/navigation";
 
 export default function CTA() {
     return (
@@ -9,7 +9,7 @@ export default function CTA() {
                 <p className="font-heading text-lg uppercase text-white">
                     Interested in joining our growing network of industry partners?
                 </p>
-                <Link href="/contact">
+                <Link href="/ContactUsPage">
                     <Button className="px-4 py-1.5 font-heading text-sm uppercase text-white">
                         Let&apos;s Talk
                     </Button>