9f8a346a3edc10c77d61fe120419402baebf335b
tazim04 <35643104+tazim04@users.noreply.github.com>
2025-08-07 21:42:45 -0400

    Removed GridGlobe

--- src/app/[locale]/ContactUsPage/components/ContactDetails.tsx
+++ src/app/[locale]/ContactUsPage/components/ContactDetails.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import GridGlobe from "./GridGlobe";
 import { useTranslations } from "next-intl";
 
 const ContactDetails = () => {
