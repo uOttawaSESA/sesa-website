994187d7c9ee7303ccc0bf1e9ed8d9f8c9b8198b
tazim04 <35643104+tazim04@users.noreply.github.com>
2025-08-10 16:46:36 -0400

    Added <p> tags with consistent gaps, updated px for 2xl screens

--- src/app/[locale]/HomeComponents/FAQ/FAQ.tsx
+++ src/app/[locale]/HomeComponents/FAQ/FAQ.tsx
@@ -17,7 +17,7 @@ const FAQ = () => {
 
     return (
         <>
-            <section className="relative flex flex-col gap-8 px-8 text-white md:px-20 xl:px-32">
+            <section className="relative flex flex-col gap-8 px-8 text-white md:px-20 xl:px-32 2xl:px-96">
                 {/* Decorations */}
                 <div className="pointer-events-none select-none">
                     {/* Warm gradient */}
@@ -68,7 +68,7 @@ const FAQ = () => {
                     </p>
                 </div>
 
-                {/* Accordian */}
+                {/* Accordion */}
                 <div className="backdrop-blue-lg flex justify-center">
                     <div className="relative w-full overflow-hidden border border-purple-600">
                         <div className="clip-corner-inner">
@@ -85,107 +85,151 @@ const FAQ = () => {
                                         WHAT IS SESA?
                                     </AccordionTrigger>
                                     <AccordionContent className="tight font-sans text-base text-thistle md:text-lg">
-                                        <b>SESA</b> is the University of Ottawa{" "}
-                                        <b>Software Engineering Students Association</b>! We're an
-                                        informally and independently run student organization with
-                                        the mission of <b>enriching SEG student life</b>,{" "}
-                                        <b>promoting software engineering</b> as a field, and{" "}
-                                        <b>supporting students academically and professionally</b>{" "}
-                                        as they begin their careers.
-                                        <br />
-                                        <br />
-                                        Our initiatives are designed to help students succeed both
-                                        in and out of the classroom. These include:
-                                        <ul className="mt-2 list-disc pl-10">
-                                            <li>
-                                                <b>Professional development workshops</b> and{" "}
-                                                <b>networking events</b> for students
-                                            </li>
-                                            <li>
-                                                <b>Academic support</b> through study resources
-                                            </li>
-                                            <li>
-                                                <b>Social and community-building events</b> to bring
-                                                students together
-                                            </li>
-                                            <li>
-                                                <b>Student outreach</b> to inspire and engage future
-                                                engineers
-                                            </li>
-                                            <li>
-                                                <b>Advocacy</b> for the SEG student voice within
-                                                uOttawa
-                                            </li>
-                                        </ul>
+                                        <div className="flex flex-col gap-4">
+                                            <p>
+                                                <b>SESA</b> is the University of Ottawa{" "}
+                                                <b>Software Engineering Students Association</b>!
+                                                We're an informally and independently run student
+                                                organization with the mission of{" "}
+                                                <b>enriching SEG student life</b>,{" "}
+                                                <b>promoting software engineering</b> as a field,
+                                                and{" "}
+                                                <b>
+                                                    supporting students academically and
+                                                    professionally
+                                                </b>{" "}
+                                                as they begin their careers.
+                                            </p>
+
+                                            <p>
+                                                Our initiatives are designed to help students
+                                                succeed both in and out of the classroom. These
+                                                include:
+                                            </p>
+
+                                            <ul className="list-disc space-y-1 pl-10">
+                                                <li>
+                                                    <b>Professional development workshops</b> and{" "}
+                                                    <b>networking events</b> for students
+                                                </li>
+                                                <li>
+                                                    <b>Academic support</b> through study resources
+                                                </li>
+                                                <li>
+                                                    <b>Social and community-building events</b> to
+                                                    bring students together
+                                                </li>
+                                                <li>
+                                                    <b>Student outreach</b> to inspire and engage
+                                                    future engineers
+                                                </li>
+                                                <li>
+                                                    <b>Advocacy</b> for the SEG student voice within
+                                                    uOttawa
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </AccordionContent>
                                 </AccordionItem>
+
                                 <AccordionItem value="item-2" className="border-purple-600 px-4">
                                     <AccordionTrigger className="py-4 font-heading text-base text-white md:text-lg">
                                         WHO CAN JOIN?
                                     </AccordionTrigger>
                                     <AccordionContent className="font-sans text-base text-thistle md:text-lg">
-                                        <b>SESA events are open to everyone</b> — not just software
-                                        engineering students!
-                                        <br />
-                                        <br />
-                                        Whether you&apos;re a <b>high school student</b>,{" "}
-                                        <b>community member</b>, <b>undergraduate</b>,{" "}
-                                        <b>graduate</b>, or <b>alum</b>, you&apos;re welcome to
-                                        attend (unless stated otherwise).
-                                        <br />
-                                        <br />
-                                        <b>To become a SESA team member:</b>
-                                        <ul className="mt-2 list-disc pl-10">
-                                            <li>
-                                                <b>You must be an enrolled uOttawa student</b>{" "}
-                                                (undergraduate or graduate, full-time or part-time).
-                                            </li>
-                                            <li>
-                                                You don't have to be in the{" "}
-                                                <b>software engineering program</b> — just
-                                                passionate about the community.
-                                            </li>
-                                        </ul>
+                                        <div className="flex flex-col gap-4">
+                                            <p>
+                                                <b>SESA events are open to everyone</b> — not just
+                                                software engineering students!
+                                            </p>
+
+                                            <p>
+                                                Whether you&apos;re a <b>high school student</b>,{" "}
+                                                <b>community member</b>, <b>undergraduate</b>,{" "}
+                                                <b>graduate</b>, or <b>alum</b>, you&apos;re welcome
+                                                to attend (unless stated otherwise).
+                                            </p>
+
+                                            <div>
+                                                <p className="mb-2">
+                                                    <b>To become a SESA team member:</b>
+                                                </p>
+                                                <ul className="list-disc space-y-1 pl-10">
+                                                    <li>
+                                                        <b>
+                                                            You must be an enrolled uOttawa student
+                                                        </b>{" "}
+                                                        (undergraduate or graduate, full-time or
+                                                        part-time).
+                                                    </li>
+                                                    <li>
+                                                        You don't have to be in the{" "}
+                                                        <b>software engineering program</b> — just
+                                                        passionate about the community.
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
                                     </AccordionContent>
                                 </AccordionItem>
+
                                 <AccordionItem value="item-3" className="border-purple-600 px-4">
                                     <AccordionTrigger className="py-4 font-heading text-base text-white md:text-lg">
                                         WHAT MAKES SESA SPECIAL?
                                     </AccordionTrigger>
                                     <AccordionContent className="font-sans text-base text-thistle md:text-lg">
-                                        <b>SESA is run by students, for students</b>, with a focus
-                                        on <b>community</b>, <b>collaboration</b>, and{" "}
-                                        <b>creating opportunities</b>.
-                                        <br />
-                                        <br />
-                                        We're committed to building an <b>inclusive space</b> where
-                                        anyone can get involved, learn, and grow.
+                                        <div className="flex flex-col gap-4">
+                                            <p>
+                                                <b>SESA is run by students, for students</b>, with a
+                                                focus on <b>community</b>, <b>collaboration</b>, and{" "}
+                                                <b>creating opportunities</b>.
+                                            </p>
+
+                                            <p>
+                                                We're committed to building an{" "}
+                                                <b>inclusive space</b> where anyone can get
+                                                involved, learn, and grow.
+                                            </p>
+                                        </div>
                                     </AccordionContent>
                                 </AccordionItem>
+
                                 <AccordionItem value="item-4" className="border-purple-600 px-4">
                                     <AccordionTrigger className="py-4 font-heading text-base text-white md:text-lg">
                                         WHAT EVENTS DOES SESA HOST?
                                     </AccordionTrigger>
                                     <AccordionContent className="font-sans text-base text-thistle md:text-lg">
-                                        SESA hosts tech-related events each year at the University
-                                        of Ottawa, including{" "}
-                                        <b>
-                                            technical workshops, career fairs, meet and greets, mock
-                                            interviews, and product launches
-                                        </b>
-                                        . Many of these activities are in partnership with community
-                                        organizations, such as <b>Blackberry</b>, <b>Shopify</b>,{" "}
-                                        <b>SurveyMonkey</b>, and <b>Warp</b> to help organize high
-                                        quality experiences comparable to the professional
-                                        workplace. For organizations or identities interested in
-                                        collaborating with us, visit our{" "}
-                                        <Link
-                                            href="/SponsorsPage"
-                                            className="underline hover:text-white"
-                                        >
-                                            Sponsor page
-                                        </Link>{" "}
-                                        for more information.
+                                        <div className="flex flex-col gap-4">
+                                            <p>
+                                                SESA hosts tech-related events each year at the
+                                                University of Ottawa, including{" "}
+                                                <b>
+                                                    technical workshops, career fairs, meet and
+                                                    greets, mock interviews, and product launches
+                                                </b>
+                                                .
+                                            </p>
+
+                                            <p>
+                                                Many of these activities are in partnership with
+                                                community organizations, such as <b>Blackberry</b>,{" "}
+                                                <b>Shopify</b>, <b>SurveyMonkey</b>, and <b>Warp</b>{" "}
+                                                to help organize high quality experiences comparable
+                                                to the professional workplace.
+                                            </p>
+
+                                            <p>
+                                                For organizations or identities interested in
+                                                collaborating with us, visit our{" "}
+                                                <Link
+                                                    href="/SponsorsPage"
+                                                    className="underline hover:text-white"
+                                                >
+                                                    Sponsor page
+                                                </Link>{" "}
+                                                for more information.
+                                            </p>
+                                        </div>
                                     </AccordionContent>
                                 </AccordionItem>
                             </Accordion>
