ff4b958756f3cb05154734caabaecb0f8d0281b3
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-08-09 23:29:48 -0400

    fix(policies): Move heading content to i18n file

--- src/app/[locale]/policies/page.tsx
+++ src/app/[locale]/policies/page.tsx
@@ -6,7 +6,7 @@ export default function Policies() {
     return (
         <div className="container mx-auto mb-16 mt-8 flex min-h-96 max-w-5xl flex-col gap-4 text-white">
             <h1 className="font-heading text-3xl uppercase md:text-5xl">
-                Our <span className="highlight-text">Policies</span>
+                {t("our_policies")} <span className="highlight-text">{t("our_policies_hl")}</span>
             </h1>
             <section>
                 <h2 id="terms" className="font-heading text-2xl uppercase md:text-4xl">
