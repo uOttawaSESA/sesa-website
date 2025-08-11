4d89a25622b790c341d7ef299fa5d71dae12ab3d
tazim04 <35643104+tazim04@users.noreply.github.com>
2025-08-11 17:59:01 -0400

    Removed inconsistent nullable typing for Quotes type

--- src/app/[locale]/HomeComponents/QuotesSection/types/Quote.tsx
+++ src/app/[locale]/HomeComponents/QuotesSection/types/Quote.tsx
@@ -2,7 +2,7 @@ export default interface Quote {
     heading: { en: string; fr: string };
     quote_part1: { en: string; fr: string };
     highlightQuote: { en: string; fr: string };
-    quote_part2: { en: string; fr: string | null };
+    quote_part2: { en: string; fr: string };
     buttonLink: string;
     memberImg: string;
     memberName: string;
