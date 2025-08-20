cfed0e1cecfa5c384c378ccce11279e5a4e1b3aa
tazim04 <35643104+tazim04@users.noreply.github.com>
2025-08-19 21:52:18 -0400

    Updated comments in firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Initialize firebase connection
 const firebaseConfig = {
-    // Your Firebase config object
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
