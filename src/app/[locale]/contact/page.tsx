5ea2d2add497a6649788b34ff8fbd66ff5a68d67
tazim04 <35643104+tazim04@users.noreply.github.com>
2025-09-01 12:05:55 -0400

    Added backdround decorations for contact page

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,6 +1,7 @@
 import ContactForm from "./components/ContactForm";
 import ContactDetails from "./components/ContactDetails";
 import FadeInSection from "@/components/FadeInSection";
+import Image from "next/image";
 
 import type { Metadata } from "next";
 
@@ -28,6 +29,37 @@ export const metadata: Metadata = {
 const Contact: React.FC = () => {
     return (
         <div className="relative min-h-screen">
+            {/* Decorations */}
+            <div className="pointer-events-none absolute inset-0">
+                {/* Light gradient */}
+                <div className="fade-from-left-bg absolute -top-10 hidden h-[110vh] w-[90vw] bg-blueviolet-100 bg-opacity-20 blur-sm md:block" />
+
+                <Image
+                    src="/decoration/star-faded.svg"
+                    className="absolute bottom-[60rem] right-10 rotate-[22deg] transform opacity-60 md:bottom-[32vh] md:left-[33vw] md:block 2xl:bottom-[40vh] 2xl:left-[20vw]"
+                    width={55}
+                    height={55}
+                    alt=""
+                />
+
+                <Image
+                    src="/decoration/star.svg"
+                    className="absolute hidden rotate-[-10deg] opacity-70 md:left-[25rem] md:top-[35rem] md:block md:h-max md:opacity-100"
+                    width={120}
+                    height={120}
+                    alt=""
+                />
+                <div className="relative -bottom-[58rem] h-[60%] overflow-hidden md:-bottom-[30rem] md:left-0 md:block">
+                    <Image
+                        src="/decoration/globe.svg"
+                        className="fade-from-top-bottom-bg-globe opacity-30 md:opacity-80"
+                        width={500}
+                        height={500}
+                        alt=""
+                    />
+                </div>
+            </div>
+
             <div className="absolute inset-0" />
 
             <div className="md:pt-26 relative px-8 pb-16 pt-10 font-heading text-white md:px-12 md:pb-20 md:pt-24 lg:px-16 xl:px-24">
