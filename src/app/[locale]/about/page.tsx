ffad7c3d143daffd35ae8039fe569e7476600041
Asad Ali <masadbali190@gmail.com>
2025-09-07 14:17:37 -0400

    fixed typo

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -16,7 +16,7 @@ import type { Metadata } from "next";
 export const generateStaticParams = localeParams;
 
 export const metadata: Metadata = {
-    title: "About | Software Engineering Student's Association",
+    title: "About | Software Engineering Students' Association",
     description: "The about page for the University of Ottawa's SESA.",
     alternates: {
         canonical: "/about",
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
         },
     },
     openGraph: {
-        title: "About | Software Engineering Student's Association",
+        title: "About | Software Engineering Students' Association",
         description: "The about page for the University of Ottawa's SESA.",
         url: new URL("https://sesa-aegl.ca/about"),
     },
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,7 +14,7 @@ import type { Metadata } from "next";
 export const generateStaticParams = localeParams;
 
 export const metadata: Metadata = {
-    title: "Home | Software Engineering Student's Association",
+    title: "Home | Software Engineering Students' Association",
     alternates: {
         canonical: "/",
         languages: {
