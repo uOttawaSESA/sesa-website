4fd988bf94d600ad757b434f593e6c3fbcddaa2e
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-09-07 10:24:43 -0400

    build: Add Nix Flake
    For convenience when setting up a dev environment through Nix. This can
    be ignored for most users; it's just for the convenience of myself and
    anybody else who prefers Nix.
    
    The new Nix files are also added to the CODEOWNERS so that DevOps
    members must approve changes.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "uOttawa SESA Main Website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_22
+          nodePackages.pnpm
+        ];
+      };
+    });
+}
