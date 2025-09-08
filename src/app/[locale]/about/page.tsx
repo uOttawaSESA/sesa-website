uOttawaSESA/sesa-website

commit a3efe2166fac7f36efbef72984038378074bbb09

feat(about): Fix "what we do" cards on mobile (#217)

src/app/[locale]/about/page.tsx
@@ -70,7 +70,7 @@ export default function About() {
     const tOurTeam = useTranslations("about.introducing_our_team_section");
 
     return (
-        <div className="min-h-screen text-white relative ">
+        <div className="min-h-screen text-white relative">
             {/* Gradient */}
             <div className="pointer-events-none absolute left-0 top-0 h-full w-full select-none">
                 {/* Light gradient */}
@@ -277,7 +277,7 @@ export default function About() {
                     </FadeInSection>
                     {/* Cards for "What do we do" */}
                     <FadeInSection>
-                        <div className="mb-8 flex w-full max-w-[28rem] gap-6 md:justify-start">
+                        <div className="mx-0 mb-8 flex w-full max-w-[28rem] gap-3 md:gap-6 md:justify-start overflow-x-auto">
                             <WhatWeDoCard
                                 imageHref="/imgs/about/social-events.webp"
                                 icon={

src/app/[locale]/about/WhatWeDoCard.tsx
@@ -20,7 +20,9 @@ export default function WhatWeDoCard({
     linkHref,
 }: WhatWeDoCardProps) {
     return (
-        <div className="outline-gradient w-fit backdrop-blur-lg">
+        // Setting a max width of 85vw makes sure that the user can see the next card, indicating that there is something to swipe to.
+        // Otherwise, it would be hard to notice that there are more cards
+        <div className="outline-gradient w-fit max-w-[85vw] backdrop-blur-lg">
             <Image
                 src={imageHref}
                 width={400}

commit 604e99f43b1992cda1c4c52be6723bd3bda15790

fix(about): Ensure that "what we do" cards work on desktop
The desktop version was accidentally broken while adjusting the mobile
version.

Fixes: a3efe21 (feat(about): Fix "what we do" cards on mobile (#217))

src/app/[locale]/about/page.tsx
@@ -277,7 +277,7 @@ export default function About() {
                     </FadeInSection>
                     {/* Cards for "What do we do" */}
                     <FadeInSection>
-                        <div className="mx-0 mb-8 flex w-full max-w-[28rem] gap-3 md:gap-6 md:justify-start overflow-x-auto">
+                        <div className="mx-0 mb-8 flex w-max max-w-[28rem] md:max-w-max gap-3 md:gap-6 md:justify-start overflow-x-auto">
                             <WhatWeDoCard
                                 imageHref="/imgs/about/social-events.webp"
                                 icon={