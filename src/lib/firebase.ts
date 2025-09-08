b35b8ed46118b2b2768fd36f448afa2912497a97
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-09-07 20:42:22 -0400

    feat(firebase): Use App Check
    Once merged, this will allow us to require reCAPTCHA verification to
    pass before users are allowed to request Firestore data.
    
    (This reCAPTCHA runs in the background; no explicit user action is
    required. It should be a seamless experience to users)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,24 @@
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
 import { getFirestore } from "firebase/firestore";
 
 // Initialize firebase connection
-const firebaseConfig = {
+const app = initializeApp({
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+});
 
-const app = initializeApp(firebaseConfig);
+const SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
+if (!SITE_KEY) throw new Error("Expected NEXT_PUBLIC_RECAPTCHA_SITE_KEY env variable.");
+
+// Enable App Check to mitigate bot requests
+initializeAppCheck(app, {
+    provider: new ReCaptchaEnterpriseProvider(SITE_KEY),
+    isTokenAutoRefreshEnabled: true,
+});
+
 export const db = getFirestore(app);
