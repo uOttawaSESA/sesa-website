411fa99e5f31e99e51c3022ffa003b8b8849d45c
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-09-08 18:53:45 -0400

    refactor: Expose query functions for TanStack Query hooks

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -10,26 +10,48 @@ import { FirestoreResource } from "@/schemas/resources";
 export const createQueryClient = () => new QueryClient();
 
 /**
+ * Query function used for {@link useEvents}.
+ */
+export const fetchEvents = async () => {
+    // Fetch from Firestore
+    const docs = (await getDocs(collection(db, "Events"))).docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id,
+    }));
+    // Include only valid events
+    const validated = docs
+        .map(doc => FirestoreEvent.safeParse(doc))
+        .filter(doc => doc.success)
+        .map(doc => doc.data);
+    return validated;
+};
+
+/**
  * Get events data from Firestore.
  * Remote data is validated with Zod before returning.
  */
 export const useEvents = () =>
     useQuery({
-        queryKey: ["eventsData"],
-        queryFn: async () => {
-            // Fetch from Firestore
-            const docs = (await getDocs(collection(db, "Events"))).docs.map(doc => ({
-                ...doc.data(),
-                id: doc.id,
-            }));
-            // Include only valid events
-            const validated = docs
-                .map(doc => FirestoreEvent.safeParse(doc))
-                .filter(doc => doc.success)
-                .map(doc => doc.data);
-            return validated;
-        },
+        queryKey: ["events"],
+        queryFn: fetchEvents,
     });
+
+/**
+ * Query function used for {@link useResources}.
+ */
+export const fetchResources = async () => {
+    // Fetch from Firestore
+    const docs = (await getDocs(collection(db, "Resources"))).docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id,
+    }));
+    // Include only valid resources
+    const validated = docs
+        .map(doc => FirestoreResource.safeParse(doc))
+        .filter(doc => doc.success)
+        .map(doc => doc.data);
+    return validated;
+};
 
 /**
  * Get resources data from Firestore.
@@ -37,18 +59,6 @@ export const useEvents = () =>
  */
 export const useResources = () =>
     useQuery({
-        queryKey: ["resourcesData"],
-        queryFn: async () => {
-            // Fetch from Firestore
-            const docs = (await getDocs(collection(db, "Resources"))).docs.map(doc => ({
-                ...doc.data(),
-                id: doc.id,
-            }));
-            // Include only valid resources
-            const validated = docs
-                .map(doc => FirestoreResource.safeParse(doc))
-                .filter(doc => doc.success)
-                .map(doc => doc.data);
-            return validated;
-        },
+        queryKey: ["resources"],
+        queryFn: fetchResources,
     });
