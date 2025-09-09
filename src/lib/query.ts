1c1a55d025ba6bb1402627f8e8d4d4eb4b85c185
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-09-09 19:26:31 -0400

    chore: Remove now-unused data-fetching hooks

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -1,4 +1,4 @@
-import { defaultShouldDehydrateQuery, QueryClient, useQuery } from "@tanstack/react-query";
+import { defaultShouldDehydrateQuery, QueryClient } from "@tanstack/react-query";
 import { collection, getDocs } from "firebase/firestore";
 import SuperJSON from "superjson";
 import { db } from "@/lib/firebase";
@@ -28,7 +28,8 @@ export const createQueryClient = () =>
     });
 
 /**
- * Query function used for {@link useEvents}.
+ * Function used to fetch all of the remote events from Firestore.
+ * You probably don't want to call this directly; use tRPC for requests from the frontend.
  */
 export const fetchEvents = async () => {
     // Fetch from Firestore
@@ -45,17 +46,8 @@ export const fetchEvents = async () => {
 };
 
 /**
- * Get events data from Firestore.
- * Remote data is validated with Zod before returning.
- */
-export const useEvents = () =>
-    useQuery({
-        queryKey: ["events"],
-        queryFn: fetchEvents,
-    });
-
-/**
- * Query function used for {@link useResources}.
+ * Function used to fetch all of the remote resources from Firestore.
+ * You probably don't want to call this directly; use tRPC for requests from the frontend.
  */
 export const fetchResources = async () => {
     // Fetch from Firestore
@@ -70,13 +62,3 @@ export const fetchResources = async () => {
         .map(doc => doc.data);
     return validated;
 };
-
-/**
- * Get resources data from Firestore.
- * Remote data is validated with Zod before returning.
- */
-export const useResources = () =>
-    useQuery({
-        queryKey: ["resources"],
-        queryFn: fetchResources,
-    });
