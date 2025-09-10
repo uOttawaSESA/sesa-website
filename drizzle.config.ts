fc2fa10315fff95128f9e3a096de4af88b5daf66
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-09-10 14:59:02 -0400

    refactor: Rename DB environment variable
    To match what Vercel calls the auto-generated ones.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from "drizzle-kit";
 
-const DATABASE_URL = process.env.DATABASE_URL;
-if (!DATABASE_URL) throw new Error("Expected DATABASE_URL env variable.");
+const POSTGRES_URL = process.env.POSTGRES_URL;
+if (!POSTGRES_URL) throw new Error("Expected POSTGRES_URL env variable.");
 
 export default {
     schema: "./src/server/db/schema.ts",
     dialect: "postgresql",
-    dbCredentials: { url: DATABASE_URL },
+    dbCredentials: { url: POSTGRES_URL },
 } satisfies Config;
