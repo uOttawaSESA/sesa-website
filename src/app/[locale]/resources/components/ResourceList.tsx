356295d3641050ca2ae9631d715d6a03ff060eac
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2025-09-14 17:33:23 -0400

    fix(resources): Fix "Load More" on mobile

--- src/app/[locale]/resources/components/ResourceList.tsx
+++ src/app/[locale]/resources/components/ResourceList.tsx
@@ -48,34 +48,43 @@ const ResourceList: React.FC<ResourceListProps> = ({
     };
 
     return (
-        <div className="flex justify-center md:mt-12 md:block">
-            {/* Decorations */}
-            <div className="pointer-events-none select-none">
-                <Image
-                    src="/decoration/floor-grid.svg"
-                    className="fade-from-bottom-bg md:-bottom-18 absolute -bottom-1 left-1/2 z-0 h-[196px] -translate-x-1/2 transform object-cover object-bottom" // Reduced height crops the top
-                    width={1200}
-                    height={430}
-                    alt=""
-                />
-            </div>
-            <div
-                className={
-                    isGridMode
-                        ? "grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3"
-                        : "flex flex-col gap-6"
-                }
-            >
-                {currentResources.map(resource => (
-                    <ResourceCard
-                        key={resource.id}
-                        {...resource}
-                        mode={isGridMode ? "grid" : "row"}
-                        onOpen={() => openModal(resource)}
+        <>
+            <div className="flex justify-center md:mt-12 md:block">
+                {/* Decorations */}
+                <div className="pointer-events-none select-none">
+                    <Image
+                        src="/decoration/floor-grid.svg"
+                        className="fade-from-bottom-bg md:-bottom-18 absolute -bottom-1 left-1/2 z-0 h-[196px] -translate-x-1/2 transform object-cover object-bottom" // Reduced height crops the top
+                        width={1200}
+                        height={430}
+                        alt=""
                     />
-                ))}
-            </div>
+                </div>
+                <div
+                    className={
+                        isGridMode
+                            ? "grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3"
+                            : "flex flex-col gap-6"
+                    }
+                >
+                    {currentResources.map(resource => (
+                        <ResourceCard
+                            key={resource.id}
+                            {...resource}
+                            mode={isGridMode ? "grid" : "row"}
+                            onOpen={() => openModal(resource)}
+                        />
+                    ))}
+                </div>
 
+                {selectedResource && (
+                    <ResourceModal
+                        resource={selectedResource}
+                        isOpen={!!selectedResource}
+                        onClose={closeModal}
+                    />
+                )}
+            </div>
             <div className="flex justify-center mt-8">
                 {isFetching ? (
                     <p className="rounded-md px-4 py-2 font-sans text-violet-400">
@@ -89,15 +98,7 @@ const ResourceList: React.FC<ResourceListProps> = ({
                     </p>
                 )}
             </div>
-
-            {selectedResource && (
-                <ResourceModal
-                    resource={selectedResource}
-                    isOpen={!!selectedResource}
-                    onClose={closeModal}
-                />
-            )}
-        </div>
+        </>
     );
 };
 
