2737b122bf727c77ccb7f4029a448951a6882031
Raman Gupta <gr.gupta.raman@gmail.com>
2025-09-15 19:47:17 -0400

    fix: connect with sesa section

--- src/app/[locale]/events/components/ConnectSESA.tsx
+++ src/app/[locale]/events/components/ConnectSESA.tsx
@@ -27,86 +27,86 @@ const ConnectSESA = () => {
                     delay={2}
                 />
             </div>
-            <div>
-                <div className="mx-auto max-w-7xl text-center">
-                    <div className="color-gradient font-mono">{t("connect_with_sesa")}</div>
-                    <h1 className="mt-4 text-[36px] uppercase">
-                        <span className="highlight-text">{t("stay_updated")}</span>
-                        <span className="ml-7">{t("stay_updated_hl")}</span>
-                    </h1>
+            <div className="px-8 max-w-7xl md:text-center ">
+                <div className="text-sm md:text-base color-gradient font-mono">
+                    {t("connect_with_sesa")}
+                </div>
+                <h1 className="mt-1 text-3xl md:text-5xl uppercase">
+                    <span className="highlight-text">{t("stay_updated")}</span>
+                    <span> {t("stay_updated_hl")}</span>
+                </h1>
 
-                    <p className="mx-auto mb-6 mt-6 max-w-[558px] font-mono text-[16px] text-thistle">
-                        {t("stay_updated_blurb")}
-                    </p>
+                <p className="mt-4 max-w-[558px] font-mono text-base md:text-lg text-thistle">
+                    {t("stay_updated_blurb")}
+                </p>
 
-                    <div className="mt-8 flex justify-center gap-4">
-                        <Button size="icon" variant="outline" asChild>
-                            <a
-                                href="https://www.instagram.com/uottawasesa/"
-                                target="_blank"
-                                rel="noopener"
-                            >
-                                <Image
-                                    src="/icons/instagram-plain.svg"
-                                    width={25}
-                                    height={25}
-                                    alt="Instagram Logo"
-                                />
-                            </a>
-                        </Button>
-                        <Button size="icon" variant="outline" asChild>
-                            <a
-                                href="https://discord.com/invite/atYdx5HHCs"
-                                target="_blank"
-                                rel="noopener"
-                            >
-                                <Image
-                                    src="/icons/discord-plain.svg"
-                                    width={25}
-                                    height={25}
-                                    alt="Discord Logo"
-                                />
-                            </a>
-                        </Button>
-                        <Button size="icon" variant="outline" asChild>
-                            <a href="https://github.com/uOttawaSESA" target="_blank" rel="noopener">
-                                <Image
-                                    src="/icons/github-plain.svg"
-                                    width={25}
-                                    height={25}
-                                    alt="GitHub Logo"
-                                />
-                            </a>
-                        </Button>
-                        <Button size="icon" variant="outline" asChild>
-                            <a
-                                href="https://www.linkedin.com/company/software-engineering-students-association/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <Image
-                                    src="/icons/linkedin-plain.svg"
-                                    width={25}
-                                    height={25}
-                                    alt="LinkedIn Logo"
-                                />
-                            </a>
-                        </Button>
-                        <Button size="icon" variant="outline" asChild>
-                            <a
-                                href="https://www.youtube.com/@uottawasesa527"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <Image
-                                    src="/icons/youtube-plain.svg"
-                                    width={25}
-                                    height={25}
-                                    alt="YouTube Logo"
-                                />
-                            </a>
-                        </Button>
-                    </div>
+                <div className="mt-4 flex justify-center gap-4">
+                    <Button size="icon" variant="outline" asChild>
+                        <a
+                            href="https://www.instagram.com/uottawasesa/"
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            <Image
+                                src="/icons/instagram-plain.svg"
+                                width={25}
+                                height={25}
+                                alt="Instagram Logo"
+                            />
+                        </a>
+                    </Button>
+                    <Button size="icon" variant="outline" asChild>
+                        <a
+                            href="https://discord.com/invite/atYdx5HHCs"
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            <Image
+                                src="/icons/discord-plain.svg"
+                                width={25}
+                                height={25}
+                                alt="Discord Logo"
+                            />
+                        </a>
+                    </Button>
+                    <Button size="icon" variant="outline" asChild>
+                        <a href="https://github.com/uOttawaSESA" target="_blank" rel="noopener">
+                            <Image
+                                src="/icons/github-plain.svg"
+                                width={25}
+                                height={25}
+                                alt="GitHub Logo"
+                            />
+                        </a>
+                    </Button>
+                    <Button size="icon" variant="outline" asChild>
+                        <a
+                            href="https://www.linkedin.com/company/software-engineering-students-association/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                src="/icons/linkedin-plain.svg"
+                                width={25}
+                                height={25}
+                                alt="LinkedIn Logo"
+                            />
+                        </a>
+                    </Button>
+                    <Button size="icon" variant="outline" asChild>
+                        <a
+                            href="https://www.youtube.com/@uottawasesa527"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                src="/icons/youtube-plain.svg"
+                                width={25}
+                                height={25}
+                                alt="YouTube Logo"
+                            />
+                        </a>
+                    </Button>
                 </div>
             </div>
         </>
