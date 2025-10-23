f0b462a58c3fca03859723de958cb42c6568434f
Raman Gupta <gr.gupta.raman@gmail.com>
2025-10-23 08:07:07 -0400

    feat: add env variables for server

---
+++ packages/env/src/server.ts
@@ -0,0 +1,23 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const envServer = createEnv({
+    server: {
+        NODE_ENV: z.string().default("development"),
+        POSTGRES_URL: z.url(),
+        RECAPTCHA_SECRET_KEY: z.string(),
+        EMAIL_USER: z.email(),
+        EMAIL_PASS: z.string(),
+        PORT: z.string().default("3000"),
+        VERCEL_URL: z.string().optional(),
+    },
+    runtimeEnvStrict: {
+        POSTGRES_URL: process.env.POSTGRES_URL,
+        NODE_ENV: process.env.NODE_ENV,
+        RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
+        EMAIL_USER: process.env.EMAIL_USER,
+        EMAIL_PASS: process.env.EMAIL_PASS,
+        PORT: process.env.PORT,
+        VERCEL_URL: process.env.VERCEL_URL,
+    },
+});
