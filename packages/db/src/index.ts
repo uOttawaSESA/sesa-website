1ab247d81622801cbc3028ea5255d4cf6843ce52
Raman Gupta <gr.gupta.raman@gmail.com>
2025-10-23 08:30:04 -0400

    feat (web): move index file to db package

---
+++ packages/db/src/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { envServer } from "@repo/env";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema.js";
+
+/**
+ * Cache the database connection in development.
+ * This avoids creating a new connection on every HMR update.
+ */
+const globalForDb = globalThis as unknown as {
+    connection: postgres.Sql | undefined;
+};
+
+const connection = globalForDb.connection ?? postgres(envServer.POSTGRES_URL, { prepare: false });
+if (envServer.NODE_ENV !== "production") globalForDb.connection = connection;
+
+export const db = drizzle(connection, { schema, casing: "snake_case" });
+export type Database = typeof db;
