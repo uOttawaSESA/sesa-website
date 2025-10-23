e6f81c26a2afbaa55919f4d2e255349aa36f76c0
Raman Gupta <gr.gupta.raman@gmail.com>
2025-10-23 07:47:35 -0400

    feat: add file for client env vars

---
+++ packages/env/src/client.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const envClient = createEnv({
+    client: {
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+        NEXT_PUBLIC_SUPABASE_URL: z.url(),
+        NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string(),
+    },
+    clientPrefix: "NEXT_PUBLIC_",
+    runtimeEnvStrict: {
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+    },
+});
