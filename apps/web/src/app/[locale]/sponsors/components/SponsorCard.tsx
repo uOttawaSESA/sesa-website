81c400544d0474b5114c5917236a2f671c1f4972
8x5y1a <jeff.gamache@icloud.com>
2026-03-17 15:37:18 -0400

    Fix: Using CN
    - Using cn function for safety on className

--- apps/web/src/app/[locale]/sponsors/components/SponsorCard.tsx
+++ apps/web/src/app/[locale]/sponsors/components/SponsorCard.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@repo/ui/lib/utils";
 import Image from "next/image";
 import type { Sponsor } from "./SponsorData";
 
@@ -13,7 +14,11 @@ export default function SponsorCard({ sponsor, className }: SponsorCardProps) {
             target="_blank"
             rel="noopener noreferrer"
             title={sponsor.name}
-            className={`flex items-center justify-center rounded-lg p-10 ${sponsor.size} outline-gradient backdrop-blur-lg transition hover:scale-105 ${className}`}
+            className={cn(
+                "flex items-center justify-center rounded-lg p-10 outline-gradient backdrop-blur-lg transition hover:scale-105",
+                sponsor.size,
+                className,
+            )}
         >
             <Image
                 src={sponsor.src}
